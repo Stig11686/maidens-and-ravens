c0382100abb5a1c8e66b1c89102c94eb425141c5
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-05-20 12:37:31 +0100

    add tailwind and enqueue

--- functions.php
+++ functions.php
@@ -138,9 +138,7 @@ add_action( 'widgets_init', 'maidens_and_ravens_widgets_init' );
  * Enqueue scripts and styles.
  */
 function maidens_and_ravens_scripts() {
-	wp_enqueue_style( 'maidens-and-ravens-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'maidens-and-ravens-style', 'rtl', 'replace' );
-
+	wp_enqueue_style( 'maidens-and-ravens-style', get_stylesheet_uri() . '/output.css', array(), _S_VERSION );
 	wp_enqueue_script( 'maidens-and-ravens-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,php,js}", "./template-parts/*.{php,html,js}", "./assets/js/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
