e5bd9eeb250d79f8ee37432de814c026ab4cab69
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-03 14:41:24 +0100

    stupid logo error

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -6,7 +6,7 @@
 <header class="bg-gray-900 text-white">
         <div class="container mx-auto flex justify-between items-center p-4">
             <div class="logo">
-                <a href="/" class="text-2xl font-bold"><?php esc_html( $logo_url ); ?></a>
+                <a href="/" class="text-2xl font-bold"><img src="<?php esc_html( $logo_url ); ?>" alt="Maidens and Ravens logo"></a>
             </div>
             <nav class="hidden md:flex space-x-4">
                 <?php wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'flex'));?>
