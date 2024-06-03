5087867c2133fb3d24e4eeba87b447923cf0ac0f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-03 14:50:43 +0100

    stupid logo error

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -11,7 +11,13 @@
                 <a href="/" class="text-2xl font-bold"><img src="<?php echo esc_url( $logo_url ); ?>" alt="Maidens and Ravens logo"></a>
             </div>
             <nav class="hidden md:flex space-x-4">
-                <?php wp_nav_menu(array('theme_location' => 'primary','container_class' => 'flex'));?>
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'primary',
+                        'menu_class' => 'flex'
+                        )
+                    );
+                ?>
                 <button id="hamburgerBtn" class="md:hidden text-white focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
