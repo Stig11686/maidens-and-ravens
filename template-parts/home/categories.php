ade2bd0ba64a930215568bd19b12143dd3d85f3f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-07 16:20:38 +0100

    can we get a category image?

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -19,7 +19,7 @@ if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) :
                     ?>
                     <div class="group relative">
                         <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                            <img src="<?php $image; ?>" alt="<?php echo esc_html( $category->name ); ?>" class="h-full w-full object-cover object-center">
+                            <img src="<?php echo $image; ?>" alt="<?php echo esc_html( $category->name ); ?>" class="h-full w-full object-cover object-center">
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
                             <a href="<?php echo esc_url( $category_link ); ?>">
