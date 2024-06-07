2602196dfd59e50245dc02b47559b32a116e6b86
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-07 14:39:46 +0100

    add template files to homepage

--- front-page.php
+++ front-page.php
@@ -2,5 +2,10 @@
     wp_head();
     include(get_template_directory() . '/template-parts/home/hero.php');
     include(get_template_directory() . '/template-parts/home/categories.php');
+    include(get_template_directory() . '/template-parts/home/brand-promise.php');
+    include(get_template_directory() . '/template-parts/home/founder-story.php');
+    include(get_template_directory() . '/template-parts/home/the-experience.php');
+    include(get_template_directory() . '/template-parts/home/personalised-dresses.php');
+    include(get_template_directory() . '/template-parts/global/testimonials.php');
     include(get_template_directory() . '/template-parts/global/footer.php');
     
