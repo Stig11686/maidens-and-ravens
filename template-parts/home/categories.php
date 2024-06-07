932851ae39463f9a79a379b07384b0f661da7f57
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-07 15:17:58 +0100

    thats better!

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -4,7 +4,7 @@ $categories = get_terms( 'product_cat', array(
 ) );
 
 if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) : 
-    ?>
+?>
     <div class="bg-gray-100">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
@@ -31,5 +31,9 @@ if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) :
             </div>
         <?php
         endforeach;
+        ?>
+        </div>
+    </div>
+    <?php
     endif;
 ?>
