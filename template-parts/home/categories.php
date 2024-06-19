e7a377ae40b00834ef8f9be8773d5bce41969e03
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-19 16:11:27 +0100

    add text

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -39,6 +39,15 @@ if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) :
                                 <img src="https://kitpro.site/alleora/wp-content/uploads/sites/247/2024/05/Frame-1-3-838x1024.png" alt="Frame Image" class="frame-image">
                             </div>
                         </div>
+                        <div>
+                        <h3 class="mt-6 text-sm text-gray-500">
+                            <a href="<?php echo esc_url( $category_link ); ?>">
+                            <span class="absolute inset-0"></span>
+                            <?php echo esc_html( $category->name ); ?>
+                            </a>
+                        </h3>
+                        <p class="text-base font-semibold text-gray-900"><?php echo esc_html( $category->description ); ?></p>
+                        </div>
                     </div>
 
                         <?php
