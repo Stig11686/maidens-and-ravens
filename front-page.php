66a301365740ebaf3c80de40fda27ae368612274
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-21 14:58:02 +0100

    reduce about text

--- front-page.php
+++ front-page.php
@@ -2,8 +2,9 @@
     include(get_template_directory() . '/template-parts/global/header.php');
     include(get_template_directory() . '/template-parts/home/hero.php');
     include(get_template_directory() . '/template-parts/home/categories.php');
-    include(get_template_directory() . '/template-parts/home/brand-promise.php');
     include(get_template_directory() . '/template-parts/home/founder-story.php');
+    include(get_template_directory() . '/template-parts/home/brand-promise.php');
+    
     include(get_template_directory() . '/template-parts/home/the-experience.php');
     include(get_template_directory() . '/template-parts/home/personalised-dresses.php');
     include(get_template_directory() . '/template-parts/global/testimonials.php');
--- template-parts/home/founders-story.php
+++ template-parts/home/founders-story.php
@@ -2,15 +2,13 @@
       <div class="mx-auto">
         <p class="my-6 text-xl leading-8 text-gray-700 font-semibold">Hello my Queens.<br>
 
-        I am Elizabeth and I am the founder, owner and bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
+        I am Elizabeth and I am the founder, owner and award winning bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
         <div class="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
           <p class="font-bold">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me.</p>
           <p class="mt-8">I started my journey into Bridalwear in 2019 but before that I always had a love for art, making things, sewing, knitting and vintage clothing. I had a strong interest in fashion history. Growing up in Edinburgh, I enjoyed charity shop snooping and loved buying dresses from the 60s and 70s which I'd get tailored to me. Like all of my friends, my prom dress was a 1980s secondhand bridesmaid dress - mint green with puff sleeves and lace! I wanted to look like Sarah from Labyrinth in the ballroom scene with David Bowie, complete with ribbons in my hair! I still love that look.</p>
-          <p class="mt-6">Believe it or not I am a practicing family lawyer and since 2003 I have been helping people with their divorces, financial arrangements and children matters. All very rewarding but over the years I missed a creative outlet.</p>
           <p class="mt-6">Like many of you, I became an avid watcher of Don't tell the Bride (I loved when the groom had to choose the dress) and Say Yes to the Dress. I started thinking how wonderful it would be to own my own boutique and help brides find their perfect dresses.</p>
           <p class="mt-6">I opened my original boutique in 2019 and after winning the Best Bridal Boutique in Yorkshire in 2022, 2023 ... I became fully independent in 2024 choosing to better adapt my boutique to my brides.</p>
           <p class="mt-6">I have a real passion for helping my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful. I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
-          <p class="mt-6">I love quirky, interesting dresses - vintage inspired, colour and alternative styles. That said, I also adore classic looks, beautiful ballgowns and clean, fitted contemporary styles. I have a real eye for what suits you and will suggest different options to adapt a dress to work better for you. Basically, whatever your dream, I want to help you achieve it and if you have no idea,  then that's absolutely fine too. I will never sell a dress to anyone that does not flatter them and I will never pressurise you into making a decision before you feel ready. I promise you, you're in safe hands. </p>
           <p class="text-gray-100 text-xl opacity-25">Elizabeth</p>
         </div>
       </div>
