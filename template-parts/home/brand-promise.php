38267c86159e663a5f299643bb435852a31e7180
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-21 15:11:03 +0100

    replace indigo with purple!

--- template-parts/home/brand-promise.php
+++ template-parts/home/brand-promise.php
@@ -4,7 +4,7 @@
       <div class="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-16">
         <div>
           <div>
-            <span class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+            <span class="flex h-12 w-12 items-center justify-center rounded-xl bg-purple-600">
               <svg class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
               </svg>
@@ -25,7 +25,7 @@
                 are saving a dress from ending up in landfill as well as saving their
                 pennies.</p>
             <div class="mt-6">
-              <a href="#" class="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">Get started</a>
+              <a href="#" class="inline-flex rounded-lg bg-purple-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-purple-600 hover:bg-purple-700 hover:ring-purple-700">Get started</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@
       <div class="mx-auto max-w-xl px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:px-0 lg:py-32">
         <div>
           <div>
-            <span class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-600">
+            <span class="flex h-12 w-12 items-center justify-center rounded-xl bg-purple-600">
               <svg class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
               </svg>
@@ -74,7 +74,7 @@ not mass produce and simply make dresses to order to a bride’s
 requirements. They enable us to be fully inclusive by being available to
 order in any size and with custom features.</p>
             <div class="mt-6">
-              <a href="#" class="inline-flex rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">Get started</a>
+              <a href="#" class="inline-flex rounded-lg bg-purple-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-purple-600 hover:bg-purple-700 hover:ring-purple-700">Get started</a>
             </div>
           </div>
         </div>
--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -19,7 +19,7 @@ $hero_image = get_theme_mod('hero_image');
         <p class="mt-6 text-2xl font-semibold leading-8 text-gray-300">Discover the magic at Maidens & Ravens,
 a unique bridal boutique in the heart of York.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="#" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Book an Appointment</a>
+          <a href="#" class="rounded-md bg-purple-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Book an Appointment</a>
           <a href="/products/all/" class="text-sm font-semibold leading-6 text-white">View Our Dresses <span aria-hidden="true">→</span></a>
         </div>
       </div>
