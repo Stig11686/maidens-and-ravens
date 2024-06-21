77740c31bfe5b2b012abe30f506fea13f775d29c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-21 13:48:13 +0100

    add testimonial post type

--- template-parts/global/testimonials.php
+++ template-parts/global/testimonials.php
@@ -15,16 +15,16 @@ $testimonials = new WP_Query([
       <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We have worked with thousands of amazing people</p>
     </div>
     <div class="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
-      <div class="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
+      <div class="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3 flex flex-col sm:flex-row">
         <?php while($testimonials->have_posts() ) : $testimonials->the_post(); ?>
           <div class="pt-8 sm:inline-block sm:w-full sm:px-4">
             <figure class="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
               <blockquote class="text-gray-900">
-                <p><?php the_content(); ?></p>
+                <p class="text-lg"><?php the_content(); ?></p>
               </blockquote>
               <figcaption class="mt-6 flex items-center gap-x-4">
                 <div>
-                  <div class="font-semibold text-gray-900"><?php the_title();?></div>
+                  <div class="font-semibold text-gray-900 text-lg"><?php the_title();?></div>
                 </div>
               </figcaption>
             </figure>
