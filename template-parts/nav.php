23bb6270b2eb643284526374cb52973038f72cbb
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-12 12:36:06 +0100

    start nav menu

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -9,13 +9,13 @@
     <div><?php echo $menu; ?></div>
     <div><img class="w-32 h-8" src="<?php echo $logo_url; ?>" alt="maidens and ravens logo" /></div>
     <div>socials icons<div>
-
+    <div class="fixed inset-0 z-10 bg-white"></div>
 </nav>
 
 
 
 
-    <script>
+    <!-- <script>
         const hamburgerBtn = document.getElementById('hamburgerBtn');
         const mobileMenu = document.getElementById('mobileMenu');
         const closeMenuBtn = document.getElementById('closeMenuBtn');
@@ -33,4 +33,4 @@
         closeMenuBtn.addEventListener('click', () => {
             mobileMenu.classList.add('hidden');
         });
-    </script>
+    </script> -->
