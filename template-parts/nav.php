66220322a731e502ecbc70e5ef6743422286c4c7
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-12 14:20:42 +0100

    start work on slide out menu

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -5,18 +5,18 @@
 
 
 <nav class="flex justify-between items-center gap-8 py-8 max-w-screen-2xl mx-auto text-white">
-    <div><img class="w-32 h-8" src="<?php echo $logo_url; ?>" alt="maidens and ravens logo" /></div>    
     <div class="flex items-center">
         <svg viewBox="0 0 12 10" class="hamburger me-3 me-xl-5 me-0 mt-1" height="20px" width="24px" class="z-50">
             <path d="M10,2 L2,2" class="upper" style="stroke: rgb(255, 255, 255); transform-origin: 0px 0px;" data-svg-origin="2 2" transform="matrix(1,0,0,1,0,0)"></path>
             <path d="M10,5 L2,5" class="lower" style="stroke: rgb(255, 255, 255); transform-origin: 0px 0px;" data-svg-origin="2 5" transform="matrix(1,0,0,1,0,0)"></path>
         </svg>
     </div>
-    
+    <div><img class="w-32 h-8" src="<?php echo $logo_url; ?>" alt="maidens and ravens logo" /></div>    
     <div>socials icons</div>
     <div id="side-menu" class="fixed h-full top-0 left-0 w-0 opacity-0 hidden">
         <div class="relative h-full">
-            <div class="overlay opacity-0 absolute top-0 left-0 w-full h-full bg-white/20"></div>
+            <div class="absolute top-0 right-0 text-white font-bold">CLOSE</div>
+            <div class="overlay opacity-0 absolute top-0 left-0 w-full h-full bg-black/20"></div>
             <div class="side-menu z-10 bg-white p-8 h-full w-full">
                 <?php wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'flex fex-col list-none ml-auto', 'add_li_class' => 'text-white px-4')); ?>
             </div>
