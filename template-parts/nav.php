a03a8f1d6df737bdbe389608dd409201ca8912b5
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-12 12:37:37 +0100

    start nav menu

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -1,12 +1,11 @@
 <?php
     $logo_id = get_theme_mod('custom_logo');
     $logo_url = wp_get_attachment_image_url($logo_id, 'full');
-    $menu = wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'flex'));
 ?>
 
 
 <nav class="flex justify-between items-center gap-8">
-    <div><?php echo $menu; ?></div>
+    <div><?php wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'flex')); ?></div>
     <div><img class="w-32 h-8" src="<?php echo $logo_url; ?>" alt="maidens and ravens logo" /></div>
     <div>socials icons<div>
     <div class="fixed inset-0 z-10 bg-white"></div>
