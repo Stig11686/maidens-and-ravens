6bf48a9b43827884a1134f33d82f45be58f8ef93
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-15 13:51:46 +0100

    add slick slider to functions php

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -17,8 +17,8 @@
         <div class="relative h-full">
 
             <div class="overlay opacity-0 absolute top-0 left-0 w-full h-full bg-black/20"></div>
-            <div class="relative side-menu z-10 bg-white p-8 h-full w-full">
-                <img src="<?php echo get_stylesheet_directory_uri(  ) . '/img/maiden-and-raven.jpg'; ?>" class="raven-image opacity-20 h-full w-full object-cover" alt="">
+            <div class="relative side-menu z-10 bg-white h-full w-full">
+                <img src="<?php echo get_stylesheet_directory_uri(  ) . '/img/maiden-and-raven.jpg'; ?>" class="raven-image opacity-20 absolute inset-0 h-full w-full object-cover" alt="">
                 <?php wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'flex flex-col gap-8 my-auto list-none ml-auto', 'add_li_class' => 'text-black px-4')); ?>
             </div>
         </div>    
