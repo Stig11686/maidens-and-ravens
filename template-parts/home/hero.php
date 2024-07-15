84c7e6f5d661f4b42f36f95f3933fe99b98367b8
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-15 15:26:10 +0100

    update nav#

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -13,9 +13,9 @@ $hero_image = get_theme_mod('hero_image');
     <img src="<?php echo $hero_image; ?>" alt="" class="absolute inset-0 h-full w-full object-cover">
     <div class="absolute inset-x-0 inset-y-0 overflow-hidden blur-3xl bg-gray-900/75" aria-hidden="true">
     </div>
-    <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-56">
+    <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-40">
       <div class="text-center z-50">
-        <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Maidens and Ravens</h1>
+        <h1 class="text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-6xl">Welcome to Maidens and Ravens</h1>
         <p class="mt-6 text-2xl font-semibold leading-8 text-gray-300">Discover the magic at Maidens & Ravens,
 a unique bridal boutique in the heart of York.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
