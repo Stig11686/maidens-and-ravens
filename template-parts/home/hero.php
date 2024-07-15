2adfb667c2a61351d7460eab1c07491c807aff09
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-15 16:19:24 +0100

    add slate background

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -17,12 +17,14 @@ $hero_image = get_theme_mod('hero_image');
       <div class="text-center z-50">
         <h1 class="text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-6xl">Welcome to<br> Maidens and Ravens</h1>
         <p class="mt-6 text-2xl font-semibold leading-8 text-gray-300">Discover the magic at Maidens & Ravens,
-a unique bridal boutique in the heart of York.</p>
+          a unique bridal boutique in the heart of York.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#" class="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Book an Appointment</a>
           <a href="/products/all/" class="text-sm font-semibold leading-6 text-white">View Our Dresses <span aria-hidden="true">→</span></a>
         </div>
       </div>
     </div>
+    <div class="overlay-top"></div>
+    <div class="overlay-bottom"></div>
   </div>
 </div>
