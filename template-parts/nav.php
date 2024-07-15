65dfe13c4f020f50cf7b98ef26469d76ce3a3a21
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-15 14:42:01 +0100

    hope new bg image on menu works

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -12,13 +12,17 @@
         </svg>
     </div>
     <div><img class="w-32 h-12" src="<?php echo $logo_url; ?>" alt="maidens and ravens logo" /></div>    
-    <div class="hidden md:block">socials icons</div>
+    <div class="hidden md:block">
+        <div class="flex gap-8">
+            <a href="https://www.facebook.com" target="_blank"><img src="<?php echo get_template_directory_uri();?>/img/socials/facebook.svg" alt="Visit the Maidens and Ravens Facebook page" /></a>
+            <a href="https://www.instagram.com" target="_blank"><img src="<?php echo get_template_directory_uri();?>/img/socials/instagram.svg" alt="Visit the Maidens and Ravens Instagram page" /></a>
+        </div>
+    </div>
     <div id="side-menu">
         <div class="relative h-full">
 
             <div class="overlay opacity-0 absolute top-0 left-0 w-full h-full bg-black/20"></div>
             <div class="relative side-menu z-10 bg-white h-full w-full">
-                <img src="<?php echo get_stylesheet_directory_uri(  ) . '/img/maiden-and-raven.jpg'; ?>" class="raven-image opacity-20 h-full w-full object-cover" alt="">
                 <?php wp_nav_menu(array('theme_location' => 'primary','menu_class' => 'absolute inset-20 flex flex-col gap-8 my-auto list-none ml-auto', 'add_li_class' => 'text-black px-4')); ?>
             </div>
         </div>    
