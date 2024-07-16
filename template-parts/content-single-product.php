5c5715fd444ea5a4f221ae65532b3aa04bf2a13e
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 15:03:24 +0100

    add book appointment button on single product page

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -7,9 +7,10 @@ global $product;
         <div class="w-full md:w-1/2">
             <img src="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ); ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover">
         </div>
-        <div class="w-full md:w-1/2 flex flex-col gap-8 p-12">
-            <h2 class="text-4xl lg:text-5xl text-primary"><?php the_title(); ?></h2>
+        <div class="w-full md:w-1/2 flex flex-col gap-8 p-12 text-center">
+            <h2 class="text-4xl lg:text-5xl text-purple-900 px-4 py-5"><?php the_title(); ?></h2>
             <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
+            <a href="#" class="border border-1 rounded border-purple-900">Click to Book an Appointment</a>
         </div>
     </div>
     <div class="w-full flex">
