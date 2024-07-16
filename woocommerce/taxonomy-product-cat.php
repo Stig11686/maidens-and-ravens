41c8ca759107be043694dd9dbfd4cbea3b4ddb41
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 10:38:06 +0100

    change file structure for woocommerce templates

--- woocommerce/taxonomy-product-cat.php
+++ woocommerce/taxonomy-product-cat.php
@@ -1,7 +1,12 @@
 <?php
 
     include(get_template_directory() . '/template-parts/global/header.php');
+    include(get_template_directory() . '/template-parts/home/hero.php');
 
 ?>
 
 testing the theory
+
+<?php
+
+    include(get_template_directory() . '/template-parts/home/footer.php');
