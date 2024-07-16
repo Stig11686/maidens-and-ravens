ff03b5eaa0517ec01a95918c8eb376518a422f8b
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 15:11:03 +0100

    add book appointment button on single product page

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -10,7 +10,7 @@ global $product;
         <div class="w-full md:w-1/2 flex flex-col gap-8 p-12 text-center">
             <h2 class="text-4xl lg:text-5xl text-purple-900"><?php the_title(); ?></h2>
             <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
-            <a href="#" class="border border-1 rounded border-purple-900 px-4 py-5 outline-1 -outline-offset-2 outline-purple-900 uppercase">Click to Book an Appointment</a>
+            <a href="#" class="border border-1 rounded border-purple-900 px-4 py-5 outline outline-1 -outline-offset-2 outline-purple-900 uppercase">Click to Book an Appointment</a>
         </div>
     </div>
     <div class="w-full flex">
