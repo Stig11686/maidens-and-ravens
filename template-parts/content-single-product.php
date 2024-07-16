a7cbc8aea54b6b04ad905e5b71a6fbb1a043c0a6
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 14:49:47 +0100

    add image on single product page

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -1,6 +1,8 @@
-<div class="flex flex-col gap-8">
+<div class="px-6 flex flex-col gap-8">
     <div class="flex flex-col md:flex-row">
-        <div class="w-full md:w-1/2">Image goes here</div>
+        <div class="w-full md:w-1/2">
+            <img src="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ); ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover">
+        </div>
         <div class="w-full md:w-1/2">product details go here</div>
     </div>
     <div class="w-full flex">
