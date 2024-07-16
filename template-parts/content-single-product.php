33902f98e6e5031992bb5104ab3d178cb0d9d14b
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 16:12:55 +0100

    remove hover effect on submenu

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -12,7 +12,6 @@ global $product;
                 <h2 class="text-4xl lg:text-5xl text-purple-900"><?php the_title(); ?></h2>
                 <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
                 <a href="#" class="relative button button--wayra border border-1 rounded border-purple-900 px-4 py-5 outline outline-1 -outline-offset-2 outline-purple-900 uppercase" data-text="Click to Book an Appointment">Click to Book an Appointment</a>
-                <button class="">Open Project</button>
             </div>
         </div>
     </div>
