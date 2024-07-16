b7bde6fc85adaff332f0234d87ac75f2b493ba4a
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 14:53:24 +0100

    add product name and price

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -3,7 +3,7 @@ global $product;
 ?>
 
 <div class="px-6 flex flex-col gap-8">
-    <div class="flex flex-col md:flex-row">
+    <div class="flex flex-col md:flex-row gap-8">
         <div class="w-full md:w-1/2">
             <img src="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ); ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover">
         </div>
