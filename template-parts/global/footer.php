555d5eb77111264001bedce51af874fd5abc65ee
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-16 16:32:54 +0100

    experiment with maiden image in footer

--- template-parts/global/footer.php
+++ template-parts/global/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-slate-950/90 relative" aria-labelledby="footer-heading">
   <h2 id="footer-heading" class="sr-only">Footer</h2>
-  <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32" style="background-image: url(<?php echo get_stylesheet_directory_uri(  ) . '/img/maidens-and-ravens-menu-op20-removebg.png'; ?>); background-repeat: no-repeat; background-position: center; background-size: cover;">
+  <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
     <div class="xl:grid xl:grid-cols-3 xl:gap-8">
       <div class="space-y-8">
         <img class="h-7" src="<?php get_theme_mod('custom_logo'); ?>" alt="maidens and ravens logo">
