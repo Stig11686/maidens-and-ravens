455f822048fd300ee90bf7e55c028dd48297878f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-17 12:30:10 +0100

    add thumbnail to testimonial post type

--- template-parts/testimonials.php
+++ template-parts/testimonials.php
@@ -10,15 +10,10 @@
 
 ?>
 <?php if ($testimonials->have_posts()) : ?>
-<div class="bg-white py-24 sm:py-32">
+<div class="bg-purple-900 py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
-    <div class="mx-auto max-w-xl text-center">
-      <h2 class="text-lg font-semibold leading-8 tracking-tight text-primary">Testimonials</h2>
-      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We have worked with thousands of amazing people</p>
-    </div>
     <div class="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
       <div class="flex flex-wrap -mt-8 sm:-mx-4 sm:text-[0] testimonial-slider">
-      
         <?php while ($testimonials->have_posts()) : $testimonials->the_post(); ?>
         <div class="pt-8 sm:inline-block w-full md:w-6/12 sm:px-4" data-theme="emerald">
             <figure class="rounded-2xl bg-primary p-8 text-sm leading-6">
