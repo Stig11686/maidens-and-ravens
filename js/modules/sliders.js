70a4c1675c3c8710b366be2f838cd98a66d1d5f4
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-17 14:29:22 +0100

    testimonial section

--- js/modules/sliders.js
+++ js/modules/sliders.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
     jQuery('.testimonial-slider').slick({
         dots: false,
         infinite: true,
-        speed: 2000,
+        speed: 500,
         slidesToShow: 1,
         adaptiveHeight: true,
         arrows: true,
