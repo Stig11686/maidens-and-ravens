4ff1b935d9042af98f2739953c00e7ab4daa541c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-18 15:52:01 +0100

    update styles for arb value

--- template-parts/home/founder-story.php
+++ template-parts/home/founder-story.php
@@ -7,7 +7,7 @@
         <div class="relative mx-auto max-w-prose text-base lg:max-w-none">
           <figure>
             <div class="aspect-h-7 aspect-w-12 lg:aspect-none">
-              <img class="rounded-lg object-cover object-center shadow-lg" src="https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80" alt="Whitney leaning against a railing on a downtown street">
+              <img class="rounded-lg object-cover object-center shadow-lg" src="<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-3.jpg'; ?>" alt="Elizabeth helping a bride down the stairs!">
             </div>
           </figure>
         </div>
