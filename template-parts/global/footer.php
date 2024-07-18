23943125abef1c5e099b69d09c8fbcbb3256eb2c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-18 15:21:33 +0100

    remove images from testimonials

--- template-parts/global/footer.php
+++ template-parts/global/footer.php
@@ -10,7 +10,7 @@
             <span class="sr-only">Facebook</span>
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
-            </svg> lg:aspect-square
+            </svg>
           </a>
           <a href="#" class="text-gray-500 hover:text-gray-400">
             <span class="sr-only">Instagram</span>
@@ -22,18 +22,6 @@
             <span class="sr-only">X</span>
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-            </svg>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-400">
-            <span class="sr-only">GitHub</span>
-            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
-            </svg>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-400">
-            <span class="sr-only">YouTube</span>
-            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clip-rule="evenodd" />
             </svg>
           </a>
         </div>
@@ -77,22 +65,22 @@
         </div>
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
-            <h3 class="text-sm font-semibold leading-6 text-white">Company</h3>
+            <h3 class="text-sm font-semibold leading-6 text-white">About Us</h3>
             <ul role="list" class="mt-6 space-y-4">
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">About</a>
+                <a href="/about" class="text-sm leading-6 text-gray-300 hover:text-white">About</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Blog</a>
+                <a href="/blog" class="text-sm leading-6 text-gray-300 hover:text-white">Blog</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Jobs</a>
+                <a href="/shop" class="text-sm leading-6 text-gray-300 hover:text-white">Dresses</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Press</a>
+                <a href="/testimonials" class="text-sm leading-6 text-gray-300 hover:text-white">Testimonials</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Partners</a>
+                <a href="/contact" class="text-sm leading-6 text-gray-300 hover:text-white">Contact Us</a>
               </li>
             </ul>
           </div>
@@ -100,13 +88,10 @@
             <h3 class="text-sm font-semibold leading-6 text-white">Legal</h3>
             <ul role="list" class="mt-6 space-y-4">
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Claim</a>
+                <a href="/terms-conditions/" class="text-sm leading-6 text-gray-300 hover:text-white">Terms and Conditoins</a>
               </li>
               <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Privacy</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Terms</a>
+                <a href="/privacy-policy/" class="text-sm leading-6 text-gray-300 hover:text-white">Privacy</a>
               </li>
             </ul>
           </div>
@@ -114,7 +99,7 @@
       </div>
     </div>
     <div class="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-      <p class="text-xs leading-5 text-gray-400">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+      <p class="text-xs leading-5 text-gray-400">&copy; <span id="currentYear"></span> Maidens & Ravens. All rights reserved.</p>
     </div>
   </div>
 </footer>
