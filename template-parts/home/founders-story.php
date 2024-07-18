8647e4e52f357e035118c5b3ac63e127061fb31d
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-18 12:32:14 +0100

    new brand promise section

--- template-parts/home/founders-story.php
+++ template-parts/home/founders-story.php
@@ -4,12 +4,8 @@
 
         I am Elizabeth and I am the founder, owner and award winning bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
         <div class="mt-10 max-w-xl text-base leading-7 text-white lg:max-w-none">
-          <p class="font-bold">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me.</p>
-          <p class="mt-8">I started my journey into Bridalwear in 2019 but before that I always had a love for art, making things, sewing, knitting and vintage clothing. I had a strong interest in fashion history. Growing up in Edinburgh, I enjoyed charity shop snooping and loved buying dresses from the 60s and 70s which I'd get tailored to me. Like all of my friends, my prom dress was a 1980s secondhand bridesmaid dress - mint green with puff sleeves and lace! I wanted to look like Sarah from Labyrinth in the ballroom scene with David Bowie, complete with ribbons in my hair! I still love that look.</p>
-          <p class="mt-6">Like many of you, I became an avid watcher of Don't tell the Bride (I loved when the groom had to choose the dress) and Say Yes to the Dress. I started thinking how wonderful it would be to own my own boutique and help brides find their perfect dresses.</p>
-          <p class="mt-6">I opened my original boutique in 2019 and after winning the Best Bridal Boutique in Yorkshire in 2022, 2023 ... I became fully independent in 2024 choosing to better adapt my boutique to my brides.</p>
-          <p class="mt-6">I have a real passion for helping my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful. I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
-          <p class="text-gray-100 text-xl opacity-25">Elizabeth</p>
+          <p class="font-bold text-xl leading-normal">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me.</p>
+          <a href="/about" class="btn bg-purple-900  text-xl font-semibold text-white ">Read More About Me</a>
         </div>
       </div>
     </div>
