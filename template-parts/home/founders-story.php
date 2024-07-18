85f4cbd14b8a32358c4b5eca40f88bcfa0c4b90b
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-18 12:37:54 +0100

    new brand promise section

--- template-parts/home/founders-story.php
+++ template-parts/home/founders-story.php
@@ -4,9 +4,7 @@
         <p class="my-6 text-xl leading-8 text-white font-semibold">Hello my Queens.<br>
 
         I am Elizabeth and I am the founder, owner and award winning bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
-        <div class="mt-10 max-w-xl text-base leading-7 text-white lg:max-w-none">
           <p class="font-bold text-xl leading-normal">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me.</p>
           <a href="/about" class="btn bg-purple-900  text-xl font-semibold text-white ">Read More About Me</a>
-        </div>
       </div>
     </div>
