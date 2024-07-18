a0e374918d4caa0e2e056219f03d7775e077da3f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-18 13:58:20 +0100

    add boutique statement

--- template-parts/home/personalised-dresses.php
+++ template-parts/home/personalised-dresses.php
@@ -7,12 +7,14 @@
     </div>
     <div class="w-full lg:w-4/12 p-6">
         <div class="flex flex-col gap-2 sticky top-8 text-white">
-            <h2 class="text-5xl leading-normal">Personalised Wedding Dresses</h2>
-            <p class="text-lg leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero tempore officia excepturi? Porro, placeat nam quis earum quia assumenda asperiores, dolorem in nostrum fugiat totam id. Ipsum, alias voluptatibus! Rem!</p>
-            <p class="text-lg leading-normal">Eligendi impedit, id neque iusto placeat accusamus veritatis ut cum, deleniti omnis mollitia voluptas et repudiandae facilis quod. Mollitia deserunt necessitatibus quae repellendus incidunt velit facere at delectus dolor atque.</p>
-            <p class="text-lg leading-normal">Incidunt error voluptate cum nostrum ratione doloribus, possimus, obcaecati quos mollitia, similique itaque pariatur! Tempora, dicta accusantium. Consequuntur consequatur libero error? Deserunt voluptatibus nam ab quia nihil quisquam consectetur doloribus.</p>
-            <p class="text-lg leading-normal">Quam porro aliquam velit et saepe nulla necessitatibus eius ab incidunt. Dolorum labore facere suscipit deserunt ut amet odit molestias iusto sint repellat? Nostrum, praesentium nam soluta perspiciatis eaque repellendus.</p>
-            <p class="text-lg leading-normal">Cupiditate maiores eum itaque totam explicabo deleniti alias neque autem, ex doloremque, perferendis quod debitis quia accusamus laboriosam et voluptas, fuga quam ad eveniet maxime eius provident. Laboriosam, ad illum.</p>
+            <div class="flex flex-col gap-2">
+                <h2 class="text-5xl leading-normal">Personalised Wedding Dresses</h2>
+                <p class="text-lg leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero tempore officia excepturi? Porro, placeat nam quis earum quia assumenda asperiores, dolorem in nostrum fugiat totam id. Ipsum, alias voluptatibus! Rem!</p>
+                <p class="text-lg leading-normal">Eligendi impedit, id neque iusto placeat accusamus veritatis ut cum, deleniti omnis mollitia voluptas et repudiandae facilis quod. Mollitia deserunt necessitatibus quae repellendus incidunt velit facere at delectus dolor atque.</p>
+                <p class="text-lg leading-normal">Incidunt error voluptate cum nostrum ratione doloribus, possimus, obcaecati quos mollitia, similique itaque pariatur! Tempora, dicta accusantium. Consequuntur consequatur libero error? Deserunt voluptatibus nam ab quia nihil quisquam consectetur doloribus.</p>
+                <p class="text-lg leading-normal">Quam porro aliquam velit et saepe nulla necessitatibus eius ab incidunt. Dolorum labore facere suscipit deserunt ut amet odit molestias iusto sint repellat? Nostrum, praesentium nam soluta perspiciatis eaque repellendus.</p>
+                <p class="text-lg leading-normal">Cupiditate maiores eum itaque totam explicabo deleniti alias neque autem, ex doloremque, perferendis quod debitis quia accusamus laboriosam et voluptas, fuga quam ad eveniet maxime eius provident. Laboriosam, ad illum.</p>
+            </div>
         </div>
     </div>
 </div>
