ba72456f056210a7902e5687b877c86b33226677
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-19 14:59:26 +0100

    update breadcrumb styles

--- woocommerce/template-parts/archive-hero.php
+++ woocommerce/template-parts/archive-hero.php
@@ -16,7 +16,7 @@ $archive_image = get_the_archive_thumbnail();
     </div>
     <div class="mx-auto flex flex-col items-center justify-center gap-4 max-w-2xl py-32 sm:py-48 lg:py-40 px-6">
       <div class="text-center z-50">
-        <h1 class="mt-6 text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300"><?php woocommerce_page_title(); ?></h1>
+        <h1 class="mt-6 text-4xl/normal lg:text-5xl/normal font-semibold text-white"><?php woocommerce_page_title(); ?></h1>
         <div class="text-white"> <?php
             do_action( 'woocommerce_archive_description' );
             ?>
