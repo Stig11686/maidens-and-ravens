b5ff02ee03c56e8ffc4d06db26de6fd1e7c86325
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-19 14:43:50 +0100

    update breadcrumb styles

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -8,8 +8,8 @@ global $product;
             <div class="w-full md:w-1/2">
                 <img src="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ) ? wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ) : 'https://images.unsplash.com/photo-1622277430358-f4d134452e2e?q=80&w=2147&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover">
             </div>
-            <div class="w-full md:w-1/2 flex flex-col gap-8 p-12 text-center">
-                <h2 class="text-4xl lg:text-5xl text-purple-900"><?php the_title(); ?></h2>
+            <div class="w-full md:w-1/2 flex flex-col gap-8 p-12 text-center text-white">
+                <h2 class="text-4xl lg:text-5xl"><?php the_title(); ?></h2>
                 <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
                 <a href="#" class="relative button button--wayra border border-1 rounded border-purple-900 px-4 py-5 outline outline-1 -outline-offset-2 outline-purple-900 uppercase" data-text="Click to Book an Appointment">Click to Book an Appointment</a>
             </div>
