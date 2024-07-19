125bde3b17040a6a9b4eaa07159db9521d731681
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-19 15:04:35 +0100

    work on acf on each product

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -1,5 +1,7 @@
 <?php
 global $product;
+$product_id = $product->get_id();
+$fields = get_fields($product_id);
 ?>
 
 <div class="max-w-7xl mx-auto py-16">
@@ -11,8 +13,20 @@ global $product;
             <div class="w-full md:w-1/2 flex flex-col gap-8 p-12 text-center text-white">
                 <h2 class="text-4xl lg:text-5xl"><?php the_title(); ?></h2>
                 <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
+                <?php
+                if($fields):
+                ?>
+                
                 <div id="custom-product-info" class="">
+                    <?php
+                        foreach ($fields as $field_name => $value) {
+                            echo '<p><strong>' . esc_html($field_name) . ':</strong> ' . esc_html($value) . '</p>';
+                        }
+                    ?>
                 </div>
+                <?php
+                endif;
+                ?>
                 <a href="#" class="relative button button--wayra border border-1 rounded border-purple-900 px-4 py-5 outline outline-1 -outline-offset-2 outline-purple-900 uppercase" data-text="Click to Book an Appointment">Click to Book an Appointment</a>
             </div>
         </div>
