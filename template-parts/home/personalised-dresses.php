067cf4eaeb1501dc20ebeccb72d951b688030f8c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-19 09:50:52 +0100

    change photos on personalised dresses section

--- template-parts/home/personalised-dresses.php
+++ template-parts/home/personalised-dresses.php
@@ -1,9 +1,9 @@
 <div class="w-full flex flex-col lg:flex-row lg:justify-between">
     <div class="w-full lg:w-1/2 p-6 flex flex-col gap-1 bg-blue-900">
         <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-1.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
-        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-2.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
-        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-3.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
-        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-4.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
+        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-17.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
+        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-11.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
+        <div class="w-full" style="background-image: url('<?php echo get_template_directory_uri(  ) . '/img/brides/maidens-and-ravens-bride-14.jpg'; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat; padding-top: 80%;"></div>
 
     </div>
     <div class="w-full lg:w-4/12 p-6">
