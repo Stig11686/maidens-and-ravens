81fbccc3e6c780aaf3541196eadae89951b3c08e
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-19 10:20:45 +0100

    add smaller fonts on smaller screens

--- template-parts/home/the-experience.php
+++ template-parts/home/the-experience.php
@@ -1,6 +1,6 @@
 <div class="max-w-7xl mx-auto px-6 py-32">
     <div id="the-experience" class="w-10/12 max-w-7xl text-white flex flex-col items-start gap-8 pb-16">
-        <p class="text-5xl leading-normal mb-0">"Feel confident and cherished every step of the way. Our warm, welcoming and inclusive boutique makes dress shopping the start of your wedding day"</p>
+        <p class="text-4xl/normal lg:text-5xl/normal leading-normal mb-0">"Feel confident and cherished every step of the way. Our warm, welcoming and inclusive boutique makes dress shopping the start of your wedding day"</p>
         <a href="/maidens-and-ravens-boutique/" class="btn bg-purple-900  text-xl font-semibold text-white ">Read More About the Boutique</a>
     </div>
 </div>
