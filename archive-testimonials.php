45ef2bbc58273f5ebeb3e81c10fc6f39699f1d0f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 14:30:13 +0100

    finalise footer

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -15,35 +15,31 @@ include(get_template_directory(  ) . '/template-parts/home/hero.php');
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title text-white">', '</h1>' );
-				?>
-			</header><!-- .page-header -->
+            <div class="w-full max-w-7xl mx-auto px-6 py-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+                <?php
+                /* Start the Loop */
+                while ( have_posts() ) :
+                    the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                    /*
+                    * Include the Post-Type-specific template for the content.
+                    * If you want to override this in a child theme, then include a file
+                    * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                    */
+                    get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+                endwhile;
 
-			the_posts_navigation();
+                the_posts_navigation();
 
-		else :
+                    else :
 
-			get_template_part( 'template-parts/content', 'none' );
+                        get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
-
+                endif;
+        ?>
+            </div>
 	</main><!-- #main -->
 
 <?php
