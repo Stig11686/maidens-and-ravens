aecc386df3505f601f9e5ad78862b8480c4ca6f3
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 14:40:09 +0100

    add testimonials

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -13,9 +13,12 @@ include(get_template_directory(  ) . '/template-parts/home/hero.php');
 
 	<main id="primary" class="site-main text-white">
 
+
 		<?php if ( have_posts() ) : ?>
 
-            <div class="mx-auto mt-16 grid max-w-7xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+            <div class="mx-auto my-16 grid max-w-7xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4 text-white">
+                <h2>Testimonials</h2>
+                <p>If you've had a great experience with us, we'd love to share it here - get in touch via email if you're happy to share your story!</p>
 
                 <?php
                 /* Start the Loop */
