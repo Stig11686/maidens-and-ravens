38d82112f205e5e5bcab3fa39a291602baad3690
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 14:44:39 +0100

    testimonials page

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -15,33 +15,35 @@ include(get_template_directory(  ) . '/template-parts/home/hero.php');
 
 
 		<?php if ( have_posts() ) : ?>
+            <div class="mx-auto my-16 max-w-7xl px-4 sm:mt-24 sm:px-6 lg:px-8 flex flex-col gap-8">
+                <h2 class="text-3xl lg:text-5xl text-white">Testimonials</h2>
+                <p class="text-lg text-white">If you've had a great experience with us, we'd love to share it here - get in touch via email if you're happy to share your story!</p>
 
-            <div class="mx-auto my-16 grid max-w-7xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4 text-white">
-                <h2>Testimonials</h2>
-                <p>If you've had a great experience with us, we'd love to share it here - get in touch via email if you're happy to share your story!</p>
+                <div class="mx-auto grid grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4 text-white">
 
-                <?php
-                /* Start the Loop */
-                while ( have_posts() ) :
-                    the_post();
+                    <?php
+                    /* Start the Loop */
+                    while ( have_posts() ) :
+                        the_post();
 
-                    /*
-                    * Include the Post-Type-specific template for the content.
-                    * If you want to override this in a child theme, then include a file
-                    * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-                    */
-                    get_template_part( 'template-parts/content', get_post_type() );
+                        /*
+                        * Include the Post-Type-specific template for the content.
+                        * If you want to override this in a child theme, then include a file
+                        * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                        */
+                        get_template_part( 'template-parts/content', get_post_type() );
 
-                endwhile;
+                    endwhile;
 
-                the_posts_navigation();
+                    the_posts_navigation();
 
-                    else :
+                        else :
 
-                        get_template_part( 'template-parts/content', 'none' );
+                            get_template_part( 'template-parts/content', 'none' );
 
-                endif;
-        ?>
+                    endif;
+            ?>
+                </div>
             </div>
 	</main><!-- #main -->
 
