31d16926699691c2cc08c18c3ea2820e4b9fe413
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 21:15:27 +0100

    remove padding on h1 for homepage hero

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -16,7 +16,7 @@ $hero_image = get_theme_mod('hero_image');
     <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-40">
       <div class="text-center z-50">
         <!-- <h1 class="text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-6xl">Welcome to<br> Maidens and Ravens</h1> -->
-        <h1 class="text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300">Discover the magic at Maidens & Ravens,
+        <h1 class="m-0 text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300">Discover the magic at Maidens & Ravens,
           a unique bridal boutique in the heart of York.</h1>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#" class="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm md:text-lg lg:text-xl font-semibold text-white shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Book an Appointment</a>
