47b8fa167838d7fe42dd3f17745089e0051d1140
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 14:24:41 +0100

    finalise footer

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -1,1 +1,52 @@
-archive for testimonials
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package maidens_and_ravens
+ */
+
+get_header();
+include(get_template_directory(  ) . '/template-parts/global/header.php');
+include(get_template_directory(  ) . '/template-parts/home/hero.php');
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+include(get_template_directory(  ) . '/template-parts/global/footer.php');
