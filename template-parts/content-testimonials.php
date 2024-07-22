37fc0e5c3d17f7884106b15dde0dd36edd9867e1
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-22 14:36:49 +0100

    add testimonials

--- template-parts/content-testimonials.php
+++ template-parts/content-testimonials.php
@@ -1,4 +1,4 @@
-<figure class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-gray-900/5">
+<figure class="rounded-2xl bg-card-bg p-6 shadow-lg ring-1 ring-gray-900/5">
     <blockquote class="">
     <div class="text-white"><?php the_content(); ?></p>
     </blockquote>
