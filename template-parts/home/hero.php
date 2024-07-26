d1743631d6fd7309239a748bcd577a9eb07f4424
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-26 13:29:59 +0100

    playing with the hero section

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -9,12 +9,12 @@ $hero_image = get_theme_mod('hero_image');
     <?php include(get_template_directory() . '/template-parts/nav.php');?>
   </header>
 
-  <div id="header" class="relative flex flex-col md:flex-row isolate overflow-hidden pt-14 rounded-b-[100px]">
-    <div class="maidens flex-1 flex items-center justify-center bg-black">
-      <span class="text" style="-webkit-text-fill: transparent; -webkit-text-stroke: 1px #fff;">Maidens</span>
+  <div id="header" class="relative min-h-screen flex flex-col md:flex-row isolate overflow-hidden pt-14 rounded-b-[100px]">
+    <div class="maidens flex-1 flex items-center justify-center bg-white">
+      <span class="text-3xl lg:text-5xl" style="-webkit-text-fill: transparent; -webkit-text-stroke: 1px #000;">Maidens</span>
     </div>
-    <div class="ravens flex-1 flex items-center justify-center">
-      <span class="">Ravens</span>
+    <div class="ravens flex-1 flex items-center justify-center bg-black">
+      <span class="text-3xl lg:text-5xl" style="-webkit-text-fill: transparent; -webkit-text-stroke: 1px #000;">Ravens</span>
     </div>
     <!-- <img src="<?php echo $hero_image; ?>" alt="" class="absolute inset-0 h-full w-full object-cover"> -->
     <!-- <div class="absolute inset-x-0 inset-y-0 overflow-hidden bg-overlay-color" aria-hidden="true">
