31ca1ae19d93faad12e1e4fd826ef0f03f6f4fbc
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-26 16:15:03 +0100

    playing with the hero section

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -9,7 +9,7 @@ $hero_image = get_theme_mod('hero_image');
 </header>
 
 <div class="relative min-h-screen bg-white">
-  <div class="flex flex-col md:flex-row gap-8 items-center justify-center min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <!-- <div class="flex flex-col md:flex-row gap-8 items-center justify-center min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex flex-col gap-8 items-center">
       <h1 class="m-0 text-4xl/normal lg:text-5xl/normal font-semibold">Discover the magic at Maidens & Ravens,
           a unique, sustainable and inclusive bridal boutique in the heart of York.</h1>
@@ -26,16 +26,16 @@ $hero_image = get_theme_mod('hero_image');
         <img src="<?php echo $hero_image ;?>" alt="" class="w-full h-full object-cover rounded-md">
       </div>
     </div>
-  </div>  
+  </div>   -->
 
-  <!-- <div id="header" class="relative isolate overflow-hidden pt-14 rounded-b-[100px]">
+  <div id="header" class="relative isolate overflow-hidden pt-14 rounded-b-[100px]">
     <img src="<?php echo $hero_image; ?>" alt="" class="absolute inset-0 h-full w-full object-cover">
     <div class="absolute inset-x-0 inset-y-0 overflow-hidden bg-overlay-color" aria-hidden="true">
     </div>
     <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-40">
       <div class="text-center z-50">
         <!-- <h1 class="text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-6xl">Welcome to<br> Maidens and Ravens</h1>
-        <h1 class="m-0 text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300">Discover the magic at Maidens & Ravens,
+        <h1 class="m-0 text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300">Discover the magic at Maidens & Ravens, -->
           a unique bridal boutique in the heart of York.</h1>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#" class="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm md:text-lg lg:text-xl font-semibold text-white shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Book an Appointment</a>
@@ -43,5 +43,5 @@ $hero_image = get_theme_mod('hero_image');
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 </div>
