93760495408c9a8c6d739dd9f871644e8469b1aa
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-26 13:44:12 +0100

    playing with the hero section

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -4,7 +4,7 @@ $hero_image = get_theme_mod('hero_image');
 
 ?>
 
-<header class="fixed inset-x-0 top-0 z-50">
+<header class="fixed inset-x-0 top-0 z-50 bg-black">
     <?php include(get_template_directory() . '/template-parts/nav.php');?>
 </header>
 
