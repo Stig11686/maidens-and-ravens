de7a983a787e83dcd66433f8582aa966705dd506
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-07-26 13:24:34 +0100

    playing with the hero section

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -10,10 +10,16 @@ $hero_image = get_theme_mod('hero_image');
   </header>
 
   <div id="header" class="relative isolate overflow-hidden pt-14 rounded-b-[100px]">
-    <img src="<?php echo $hero_image; ?>" alt="" class="absolute inset-0 h-full w-full object-cover">
-    <div class="absolute inset-x-0 inset-y-0 overflow-hidden bg-overlay-color" aria-hidden="true">
+    <div class="maidens flex-1 flex items-center justify-center bg-black">
+      <span class="text" style="-webkit-text-fill: transparent; -webkit-text-stroke: 1px #fff;">Maidens</span>
     </div>
-    <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-40">
+    <div class="ravens flex-1 flex items-center justify-center">
+      <span class="">Ravens</span>
+    </div>
+    <!-- <img src="<?php echo $hero_image; ?>" alt="" class="absolute inset-0 h-full w-full object-cover"> -->
+    <!-- <div class="absolute inset-x-0 inset-y-0 overflow-hidden bg-overlay-color" aria-hidden="true">
+    </div> -->
+    <!-- <div class="mx-auto flex flex-col items-center justify-center gap-8 max-w-2xl py-32 sm:py-48 lg:py-40">
       <div class="text-center z-50">
         <!-- <h1 class="text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-6xl">Welcome to<br> Maidens and Ravens</h1> -->
         <h1 class="m-0 text-4xl/normal lg:text-5xl/normal font-semibold text-gray-300">Discover the magic at Maidens & Ravens,
@@ -23,6 +29,6 @@ $hero_image = get_theme_mod('hero_image');
           <a href="/products/all/" class="text-sm md:text-lg lg:text-xl font-semibold leading-6 text-white">View Our Dresses <span aria-hidden="true">→</span></a>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </div>
