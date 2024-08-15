25659884a997e8a2ee1167a7ef626067d6ee77c3
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-15 21:01:13 +0100

    customisable hero images

--- inc/customizer.php
+++ inc/customizer.php
@@ -62,7 +62,7 @@ add_action( 'customize_preview_init', 'maidens_and_ravens_customize_preview_js' 
 
 function theme_customiser_settings($wp_customize){
 	$wp_customize->add_section('home_page_section', array(
-		'title' => __('Home Page', 'maidens-and-ravens'),
+		'title' => __('Home Page', 'handp24'),
 		'priority' => 30
 	));
 
@@ -73,19 +73,19 @@ function theme_customiser_settings($wp_customize){
 
 
 	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_image_control', array(
-		'label' => __('Select Banner Image 1', 'maidens-and-ravens'),
+		'label' => __('Select Banner Image 1', 'handp24'),
 		'section' => 'home_page_section',
 		'settings' => 'hero_image_1',
 	)));
 
 	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_image_control', array(
-		'label' => __('Select Banner Image 2', 'maidens-and-ravens'),
+		'label' => __('Select Banner Image 2', 'handp24'),
 		'section' => 'home_page_section',
 		'settings' => 'hero_image_2',
 	)));
 
 	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_image_control', array(
-		'label' => __('Select Arch Image', 'maidens-and-ravens'),
+		'label' => __('Select Arch Image', 'handp24'),
 		'section' => 'home_page_section',
 		'settings' => 'arch_image',
 	)));
