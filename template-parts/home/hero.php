a1e0a3a2d8feef29a00c63b690058d061ef11e13
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-15 20:40:17 +0100

    move window to bottom of div

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -4,7 +4,7 @@ $tagline = get_bloginfo( 'description');
 ?>
 
 <div class="bg-white">
-  <header class="absolute inset-x-0 top-0 z-50 bg-primary">
+  <header class="absolute inset-x-0 top-0 z-50 bg-secondary">
     <?php include(get_template_directory() . '/template-parts/nav.php');?>
   </header>
   <div id="homepage-hero" class="homepage-hero relative isolate overflow-hidden">
