0b9cd13a2c78072ffa8b076264d9bb50712dc228
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-15 10:30:53 +0100

    remove window for now

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -9,7 +9,7 @@ $tagline = get_bloginfo( 'description');
   </header>
   <div id="homepage-hero" class="homepage-hero relative isolate overflow-hidden">
     <div class="ml-auto w-full pb-16 pt-40 md:pt-20">
-    <div class="hidden lg:block lg:absolute inset-2/4 rounded-t-full border-8 border-white h-[53%] w-[17%] -translate-y-1/4 -translate-x-1/2">
+    <div class="hidden lg:absolute inset-2/4 rounded-t-full border-8 border-white h-[53%] w-[17%] -translate-y-1/4 -translate-x-1/2">
       <img src="<?php echo esc_url($hero_image); ?>" alt="" class="object-cover h-full w-full rounded-t-full">
     </div>
       <div class="flex flex-col gap-2 md:flex-row md:items-center pt-8">
@@ -18,8 +18,8 @@ $tagline = get_bloginfo( 'description');
             <h1 id="tagline" class="leading-snug max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl xl:col-auto">Discover the Magic at Maidens and Ravens</h1>
             <p class="lg:w-6/12 text-lg leading-8 text-gray-600">A unique, sustainable and inclusive bridal boutique in the heart of York</p>
             <div class="mt-10 flex items-center gap-x-6 ctas">
-                <a href="tel:07970742162" class="rounded-2xl bg-neutral px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral">Start a project now!</a>
-                <a href="/case-studies" class="text-sm font-semibold leading-6 text-gray-900">See our Work <span class="green-arrow"></span></a>
+                <a href="tel:07970742162" class="rounded-2xl bg-neutral px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral">See our Dresses</a>
+                <a href="/case-studies" class="text-sm font-semibold leading-6 text-gray-900">About our Boutique <span class="green-arrow"></span></a>
             </div>
           </div>
         </div>
