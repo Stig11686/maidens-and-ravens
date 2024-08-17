d49ea17cb68565a3bf575882d74dca0e5e65c758
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-17 20:14:00 +0100

    sort testimonial slider

--- js/modules/sliders.js
+++ js/modules/sliders.js
@@ -13,4 +13,18 @@ jQuery(document).ready(function(){
         // prevArrow: jQuery('.slider-arrows .prev'),
         // nextArrow: jQuery('.slider-arrows .next'),
     });
+
+    jQuery('.testimonial-slider').slick({
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 2000,
+        speed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: false,
+        autoplay: true,
+        cssEase: 'linear',
+        prevArrow: jQuery('.slider-arrows .prev'),
+        nextArrow: jQuery('.slider-arrows .next'),
+    });
   });
