a78f970517f87cd0d7f125f6c27a2afd7e1106e3
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-17 20:06:53 +0100

    try a new inset for the grey box effect

--- template-parts/home/founders-story.php
+++ template-parts/home/founders-story.php
@@ -1,10 +1,10 @@
 <div class="px-6 lg:contents">
       <div class="mx-auto flex flex-col items-start gap-8">
         <h2 class="mt-2 text-3xl font-bold leading-8 tracking-tight sm:text-5xl">Meet <span class="text-primary">Elizabeth</span></h3>
-        <p class="my-6 text-xl leading-8 text-white font-semibold">Hello my Queens.<br>
+        <p class="my-6 text-xl leading-8 font-semibold">Hello my Queens.<br>
 
         I am Elizabeth and I am the founder, owner and award winning bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
-          <p class="font-bold text-xl leading-normal text-white">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me...</p>
+          <p class="font-bold text-xl leading-normal">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me...</p>
           <a href="/about" class="rounded-md bg-primary px-3.5 py-2.5 text-sm md:text-lg lg:text-xl font-semibold text-white shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Read More About Me</a>
       </div>
     </div>
