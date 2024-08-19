36dafe8f19448f8887a744237968c552cf987f17
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-19 15:25:33 +0100

    add zero margin to h1

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -6,7 +6,7 @@
 ?>
 
     <div class="flex flex-col items-start gap-4 max-w-2xl lg:ml-6 px-4 pt-20">
-        <h1 class="text-3xl md:text-4xl lg:text-5xl text-black font-bold">Get Inspired</h1>
+        <h1 class="mb-0 text-3xl md:text-4xl lg:text-5xl text-black font-bold">Get Inspired</h1>
         <p class="text-xl lg:text-2xl font-bold">Decide whether you are a maiden or a raven! Browse your dream dresses, and get in touch to see them in person!</p>
     </div>
 
