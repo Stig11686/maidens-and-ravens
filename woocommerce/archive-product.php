4e3814117ff31144670d994e534f51b9cf85e328
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-19 14:51:37 +0100

    add header and footer to shop page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,3 +1,4 @@
 <?php
     include(get_template_directory(  ) . '/template-parts/global/header.php');
+    include(get_template_directory(  ) . '/woocommerce/template-parts/archive-hero.php');
     include(get_template_directory(  ) . '/template-parts/global/footer.php');
