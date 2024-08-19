a9022e211e869a449c32e154a619073283497808
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-19 15:20:22 +0100

    new shop hero

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,4 +1,18 @@
+
+<div class="bg-grey-100">
 <?php
     include(get_template_directory(  ) . '/template-parts/global/header.php');
-    include(get_template_directory(  ) . '/woocommerce/template-parts/archive-hero.php');
+?>
+
+    <div class="flex flex-col items-start gap-4">
+        <h1 class="text-3xl md:text-4xl lg:text-5xl text-black font-bold">Get Inspired</h1>
+        <p>Decide whether you are a maiden or a raven! Browse your dream dresses, and get in touch to see them in person!</p>
+    </div>
+
+
+
+<?php
     include(get_template_directory(  ) . '/template-parts/global/footer.php');
+?>
+
+</div>
