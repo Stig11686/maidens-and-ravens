e64f91688c31bd27a2bc9f518519cf239726cd66
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-23 14:44:14 +0100

    add map section with form

--- template-parts/global/map.php
+++ template-parts/global/map.php
@@ -5,7 +5,7 @@
         <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
-        <?php do_shortcode('[contact-form-7 id="080a418" title="Enquiry Form"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="080a418" title="Enquiry Form"]'); ?>
       </div>
     </div>
   </section>
