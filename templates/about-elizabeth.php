d716cdca29bb0c813e35da89dd6ec9516c34b5bf
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-23 16:05:09 +0100

    add new settings for elizabeth page customiser

--- templates/about-elizabeth.php
+++ templates/about-elizabeth.php
@@ -22,20 +22,20 @@ include(get_template_directory() . '/template-parts/global/header.php');
 
   <section class="flex pt-4 pb-8 px-6 xl:px-48 lg:px-24 mx-auto ">
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
+      <div class="bg-grey-2000 h-full w-full lg:w-6/12">
+        <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_2')); ?>" alt="lorem10" class="w-full h-full object-cover">
+      </div>
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
         <p class="text-wrap text-lg">Believe it or not I am a practising family lawyer and since 2003 I have been helping people with their divorces, financial arrangements and children matters. All very rewarding but over the years I missed a creative outlet.</p>
         <p class="text-wrap text-lg">Like many of you, I became an avid watcher of Don't tell the Bride (I loved when the groom had to choose the dress) and Say Yes to the Dress. I started imagining how wonderful it would be to own my own bridal boutique and help brides find their perfect dresses.</p>
         <p class="text-wrap text-lg">I opened my original Reloved boutique in 2019 and after winning the Best Bridal Boutique in Yorkshire in 2022, 2023 ... I became fully independent in 2024 choosing to better adapt my boutique to my brides.</p>
-      </div>
-      <div class="bg-grey-2000 h-full w-full lg:w-6/12">
-        <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_2')); ?>" alt="lorem10" class="w-full h-full object-cover">
       </div>
     </div>
   </section>
 
   <section class="flex pt-4 pb-8 px-6 xl:px-48 lg:px-24 mx-auto ">
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
-      <div class="flex flex-col items-start gap-8 decorated-list lg:order-2 lg:w-5/12">
+      <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
         <p class="text-wrap text-lg">I have a real passion for helping all of my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful and meets your dreams! I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
         <p class="text-wrap text-lg">I love quirky, interesting dresses - vintage inspired, colour and alternative styles especially dark and gothic. That said, I also adore classic looks, beautiful princess ballgowns and clean contemporary styles. I have a real eye for what suits you and will suggest different options to adapt a dress to work better for you. I will often pick out a wild card dress for you (sometimes it comes to me as soon as I meet you!) and many of my brides have actually found that it is the one.</p>
         <p class="text-wrap text-lg">Basically, whatever is your dream dress, I want to help you achieve it and if you have no idea, then that's absolutely fine too.  We will find it.</p>
