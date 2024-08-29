592c44e32a0b71a996653c9383712e6848d3089c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-29 10:00:49 +0100

    update css

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -7,7 +7,6 @@ $categories = get_terms( 'product_cat', array(
 if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) : 
 ?>
     <div class="relative">
-        <div class="absolute bg-gray-100 inset-80 left-0"></div>
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
                 <h2 class="text-5xl font-bold text-black">View our <span class="text-primary">Dresses</span></h2>
