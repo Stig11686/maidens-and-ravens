083c61f76260bea051f26de34c875e195558471f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-29 10:52:02 +0100

    change bg img

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -9,9 +9,9 @@ $tagline = get_bloginfo( 'description');
   <header class="absolute inset-x-0 top-0 z-50 bg-neutral">
     <?php include(get_template_directory() . '/template-parts/nav.php');?>
   </header>
-  <div id="homepage-hero" class="homepage-hero relative isolate overflow-hidden h-screen bg-cover bg-fixed bg-no-repeat">
+  <div id="homepage-hero" class="homepage-hero relative isolate overflow-hidden h-screen bg-cover bg-fixed bg-no-repeat flex items-center justify-center">
     <div id="background-overlay">
-    <div class="w-full">
+    <div class="w-full z-50">
       <div class="flex flex-col gap-2 md:flex-row md:items-center pt-8 lg:pt-0">
         <div class="md:w-6/12 flex flex-col items-center gap-4 z-10 px-4">
           <div class="max-w-xl mx-auto flex flex-col items-start -mt-10">
