c5f59e9c0a8b9aa5077a86dba31c07d8a0636b1f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-29 14:15:59 +0100

    play with positioning on hero box image

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -26,7 +26,7 @@ $tagline = get_bloginfo( 'description');
         <div class="hidden lg:block lg:absolute left-7 bottom-[-7rem] rounded-t-full border-8 border-white h-[53%] w-[17%]">
           <img src="<?php echo esc_url($arch_image); ?>" alt="" class="object-cover h-full w-full rounded-t-full">
         </div>
-        <div class="hidden lg:block lg:absolute rotate-12 right-7 bottom-[-7rem] border-3 border-l-8 border-white h-[33%] w-[17%]">
+        <div class="hidden lg:block lg:absolute rotate-12 right-7 bottom-[-4rem] border-2 border-l-8 border-white h-[33%] w-[17%]">
           <img src="<?php echo esc_url($arch_image); ?>" alt="" class="object-cover h-full w-full">
         </div>
       </div>
