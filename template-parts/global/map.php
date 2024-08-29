bf2f2473788831fd1786b7efc41077747c189906
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-29 14:53:25 +0100

    change width of map

--- template-parts/global/map.php
+++ template-parts/global/map.php
@@ -2,8 +2,7 @@
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:order-2 lg:w-5/12">
         <h2 class="mb-0 leading-120">Find our Boutique!</h2>
-        <div style="width: 100%"><iframe width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-        </div>
+        <iframe width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
         <?php echo do_shortcode('[contact-form-7 id="080a418" title="Enquiry Form"]'); ?>
