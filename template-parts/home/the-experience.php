fe27176858b251bf0811bd31e3030672debb0c9a
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-29 14:38:33 +0100

    change font on second cta section

--- template-parts/home/the-experience.php
+++ template-parts/home/the-experience.php
@@ -1,6 +1,6 @@
 <div class="max-w-7xl mx-auto px-6 py-32">
     <div data-aos="zoom-in" data-aos-duration="1500" data-easing="ease-in" id="the-experience" class="w-10/12 max-w-7xl flex flex-col items-start gap-8 pb-16">
-        <p class="text-3xl/normal lg:text-5xl/normal leading-normal mb-0">"Feel confident and cherished every step of the way. Our warm, welcoming and inclusive boutique makes dress shopping the start of your wedding day"</p>
+        <p class="font-beauty text-3xl/normal lg:text-5xl/normal leading-normal mb-0">"Feel confident and cherished every step of the way. Our warm, welcoming and inclusive boutique makes dress shopping the start of your wedding day"</p>
         <a href="/maidens-and-ravens-boutique/" class="rounded-md bg-primary px-3.5 py-2.5 text-sm md:text-lg lg:text-xl font-semibold text-white shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Read More About the Boutique</a>
     </div>
 </div>
