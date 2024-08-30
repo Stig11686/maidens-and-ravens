2e78b51f8c2cc1a04170185b1832133df8b232cf
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-30 14:13:00 +0100

    add styles

--- template-parts/global/internal-hero.php
+++ template-parts/global/internal-hero.php
@@ -10,7 +10,7 @@ $tagline = get_bloginfo( 'description');
   <!-- <header class="absolute inset-x-0 top-0 z-50 bg-neutral">
     <?php include(get_template_directory() . '/template-parts/nav.php');?>
   </header> -->
-  <div id="homepage-hero" class="homepage-hero relative isolate h-[50vh] bg-cover bg-fixed bg-no-repeat flex items-center justify-center">
+  <div class="homepage-hero relative isolate h-[50vh] bg-cover bg-fixed bg-no-repeat flex items-center justify-center" style="background-image: url('<?php maidens_and_ravens_post_thumbnail(); ?>')">
     <div id="background-overlay"></div>
     <div class="w-full z-50">
       <div class="flex flex-col gap-2 md:flex-row md:items-center pt-8 lg:pt-0">
