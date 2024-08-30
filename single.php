f5a98356b809ab6df2af290d899bf67171123882
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-08-30 14:04:59 +0100

    start of single blog page

--- single.php
+++ single.php
@@ -9,32 +9,39 @@
 include(get_template_directory(  ) . '/template-parts/global/header.php');
 include(get_template_directory(  ) . '/template-parts/global/internal-hero.php');
 ?>
+<div class="flex flex-col md:flex-row gap-8">
+	<div class="px-4 max-w-7xl mx-auto">
 
-	<main id="primary" class="site-main">
+		<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'maidens-and-ravens' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'maidens-and-ravens' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'maidens-and-ravens' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'maidens-and-ravens' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
+	</div>
 
 <?php
 get_sidebar();
+?>
+</div>
+<?php
 include(get_template_directory(  ) . '/template-parts/global/footer.php');
+?>
