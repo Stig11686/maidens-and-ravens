6379ae36a2066e01890ac2366582e67c5aeaa402
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-02 19:05:52 +0100

    try and build a block

--- templates/internal-static-content.php
+++ templates/internal-static-content.php
@@ -4,4 +4,10 @@ Template Name: Internal Static Page
 */
 
 include(get_template_directory(  ) .'/template-parts/global/internal-hero.php');
+
+<div class="wp-block-two-column-block">
+    <!-- The block will be dynamically rendered here when added via the editor -->
+</div>
+
+
 include(get_template_directory(  ) . 'template-parts/global/footer.php');
