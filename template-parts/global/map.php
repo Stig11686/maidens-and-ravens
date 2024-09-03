017d1e70a8346a2ee1fdd115d96bec319f675c9c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-03 05:34:31 +0100

    change wording on map section

--- template-parts/global/map.php
+++ template-parts/global/map.php
@@ -1,7 +1,7 @@
 <section class="flex pb-8 px-6 xl:px-48 lg:px-24 py-24 mx-auto">
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:order-2 lg:w-5/12">
-        <h2 class="mb-0 text-3xl lg:text-5xl">Find our Boutique!</h2>
+        <h2 class="mb-0 text-3xl lg:text-5xl">Find the Boutique!</h2>
         <iframe width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
