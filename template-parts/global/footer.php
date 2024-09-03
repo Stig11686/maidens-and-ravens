ea6f6eb3fb05178f3e65e2b891b655ec83850b2a
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-03 04:21:50 +0100

    change footer bg colour

--- template-parts/global/footer.php
+++ template-parts/global/footer.php
@@ -7,7 +7,7 @@
 
 <?php include(get_template_directory() . '/template-parts/global/map.php'); ?>
 
-<footer class="bg-transparent text-black relative" aria-labelledby="footer-heading">
+<footer class="bg-primary text-white relative" aria-labelledby="footer-heading">
   <h2 id="footer-heading" class="sr-only">Footer</h2>
   <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
     <div class="xl:grid xl:grid-cols-2 xl:gap-8">
