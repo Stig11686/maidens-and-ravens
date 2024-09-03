206c2a0803c79d2234c52985985655f077a7445e
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-03 19:50:47 +0100

    fix load more posts btn

--- template-parts/content-posts.php
+++ template-parts/content-posts.php
@@ -5,7 +5,7 @@
           <img class="h-56 w-full object-cover" src="<?php the_post_thumbnail_url( 'full' ); ?>" alt="<?php the_title(); ?>" />
         </div>
 
-        <div class="flex-1 bg-white px-4 md:px-6 py-6 flex flex-col justify-between items-start relative">
+        <div class="flex-1 bg-white px-4 md:px-6 py-6 flex flex-col gap-4 justify-between items-start relative">
           <div class="flex-1 mt-2">
             <a href="<?php the_permalink( ); ?>" class="block">
               <a href="<?php the_permalink(); ?>" class="block"><h3 class="text-primary font-bold mb-4"><?php the_title(); ?></h3></a>
