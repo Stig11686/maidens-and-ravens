d74c24992b5cb09c7781e639ff23e357146109f2
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-03 11:49:40 +0100

    add link to book appointment button

--- template-parts/global/book-appointment-btn.php
+++ template-parts/global/book-appointment-btn.php
@@ -1,1 +1,1 @@
-<a href="#" class="px-4 py-2 bg-primary text-white cursor-pointer rounded-2xl text-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Book an Appointment</a>
+<a href="#contact" class="px-4 py-2 bg-primary text-white cursor-pointer rounded-2xl text-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Book an Appointment</a>
--- template-parts/global/map.php
+++ template-parts/global/map.php
@@ -4,7 +4,7 @@
         <h2 class="mb-0 text-3xl lg:text-5xl">Find the Boutique!</h2>
         <iframe width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
-      <div class="bg-grey-2000 h-full w-full lg:w-6/12">
+      <div id="contact" class="bg-grey-2000 h-full w-full lg:w-6/12">
         <?php echo do_shortcode('[contact-form-7 id="080a418" title="Enquiry Form"]'); ?>
       </div>
     </div>
