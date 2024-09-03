7df2d7c4ab41839a3436d3defb904086d7becdd7
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-03 20:40:02 +0100

    more amends to archive product page

--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo the_title() . ' | ' . get_bloginfo('name'); ?></title>
+    <!-- <title><?php echo the_title() . ' | ' . get_bloginfo('name'); ?></title> -->
     <?php wp_head(); ?>
 </head>
 <body class="">
