80cf38c4167cc84414bcffd56b56c1c8671ab304
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-04 17:30:50 +0100

    add more space around the quote

--- template-parts/home/brand-promise.php
+++ template-parts/home/brand-promise.php
@@ -1,5 +1,5 @@
 <div class="max-w-7xl px-6 mx-auto">
-  <div class="w-10/12 max-w-7xl flex flex-col items-start gap-8 py-16">
+  <div class="w-10/12 max-w-7xl flex flex-col items-start gap-8 py-32">
     <p data-aos="zoom-in" data-aos-duration="1500" data-easing="ease-in" class="font-beauty text-3xl lg:text-5xl/normal leading-normal mb-0">"Be our guest; our stunning collection of one-off dresses are fully customisable, allowing us to add your personality, vision 
       and a sprinkle of magic to create the dress of your dreams."</p>
     <a href="/product-category/treasure-trove/" class="rounded-md bg-primary px-3.5 py-2.5 text-sm md:text-lg lg:text-xl font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">View Our Treasure Trove</a>
