8b908e600519a2564ed0089f4a2c626e03aa7aa3
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-07 21:35:35 +0100

    might not need the contact page after all ;-)

--- templates/contact.php
+++ templates/contact.php
@@ -4,5 +4,4 @@ Template Name: Contact
 */
 include(get_template_directory() . '/template-parts/global/header.php');
 include(get_template_directory() . '/template-parts/global/internal-hero.php');
-include(get_template_directory(  ) . '/template-parts/contact/contact.php');
 include(get_template_directory(  ) . '/template-parts/global/footer.php');
