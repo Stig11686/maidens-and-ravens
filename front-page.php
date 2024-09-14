5146a4042fd3ebe3a5835fb1db0953792358710b
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-14 06:07:25 +0100

    add padding around categories on front page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,12 @@
 <?php
     include(get_template_directory() . '/template-parts/global/header.php');
     include(get_template_directory() . '/template-parts/home/hero.php');
+?>
+<div class="py-16 sm:py-24 lg:py-48">
+    <?php
     include(get_template_directory() . '/template-parts/home/categories.php');
+    ?>
+</div>
     include(get_template_directory() . '/template-parts/home/brand-promise.php');
     include(get_template_directory() . '/template-parts/home/founder-story.php');
    
