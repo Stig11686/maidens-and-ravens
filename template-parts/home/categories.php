a86cac96ca42e8294a21945e8f510cfd8940f6c3
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-14 05:36:00 +0100

    adjust grid for categories page to account for real products

--- template-parts/home/categories.php
+++ template-parts/home/categories.php
@@ -11,7 +11,7 @@ if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) :
             <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-48">
                 <h2 class="text-5xl font-bold text-black">View our <span class="text-primary">Dresses</span></h2>
 
-                <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 gap-8 lg:space-y-0">
+                <div class="mt-6 space-y-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:space-y-0">
                     <?php
                     $index=0;
                         foreach ( $categories as $category ) :
