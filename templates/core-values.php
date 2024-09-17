563d3213b5a601f3aafc6455fa21697c41c3974c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-17 20:27:02 +0100

    add core values text

--- templates/core-values.php
+++ templates/core-values.php
@@ -10,11 +10,9 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
   <section class="flex pt-12 lg:pt-36 pb-8 px-6 xl:px-48 lg:px-24 mx-auto ">
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
-        <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">My Core Values</h2>
-        <p class="text-2xl">Hello my Queens!</p>
-        <p class="text-wrap text-lg">I am Elizabeth and I am the founder, owner and bridal mavern at Maidens and Ravens, here in the historic city of York. </p>
-        <p class="text-wrap text-lg">Did you know that a mavern is someone who is experienced and knowledgeable? Seemed like a fitting name for me.</p>
-        <p class="text-wrap text-lg">I started my journey into Bridalwear in 2019 but before that I always had a love for art, making things, sewing, knitting and vintage clothing. Growing up in Edinburgh, I enjoyed charity shop snooping and loved buying dresses from the 60s and 70s which I would get tailored to me. Like all of my friends, my prom dress was a 1980s bridesmaid dress - mint green with puff sleeves and lace! I wanted to look like Sarah from Labyrinth in the ballroom scene with David Bowie, complete with ribbons in my hair! I still love that look.</p>
+        <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">Caring for the Planet</h2>
+        <h3>Wedding dresses that won’t cost the earth.</h3>
+        <p class="text-wrap text-lg">I am passionate in doing what I can to offer brides a truly sustainable choice. My treasure trove dresses are brand new, sample dresses that are already in existence and deserve the opportunity of being worn! They are my most eco-friendly option and are the most budget friendly too. By choosing one of these dresses, you are able to purchase a dress off the peg with no environmental impact at all.</p>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
         <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_1')); ?>" alt="lorem10" class="w-full h-full object-cover">
@@ -28,9 +26,10 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
         <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_2')); ?>" alt="lorem10" class="w-full h-full object-cover">
       </div>
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
-        <p class="text-wrap text-lg">Believe it or not I am a practising family lawyer and since 2003 I have been helping people with their divorces, financial arrangements and children matters. All very rewarding but over the years I missed a creative outlet.</p>
-        <p class="text-wrap text-lg">Like many of you, I became an avid watcher of Don't tell the Bride (I loved when the groom had to choose the dress) and Say Yes to the Dress. I started imagining how wonderful it would be to own my own bridal boutique and help brides find their perfect dresses.</p>
-        <p class="text-wrap text-lg">I opened my original Reloved boutique in 2019 and after winning the Best Bridal Boutique in Yorkshire in 2022, 2023 ... I became fully independent in 2024 choosing to better adapt my boutique to my brides.</p>
+        <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">Affordable Dresses that make you feel like a Queen!</h2>
+        <h3>Affordable dresses that don’t compromise on style or quality!</h3>
+        <p class="text-wrap text-lg">Getting married can be a very expensive affair and costs can quickly mount! I wanted to focus my collection on dresses that didn’t compromise on style or quality but also didn’t break the bank! I cater to budgets starting from £200 to £2000 max, helping everyone to feel like a queen on their wedding day! </p>
+        <p class="text-wrap text-lg">I love quirky, interesting dresses - vintage inspired, colour and alternative styles especially dark and gothic. That said, I also adore classic looks, beautiful princess ballgowns and clean contemporary styles. I have a real eye for what suits you and will suggest different options to adapt a dress to work better for you. </p>
       </div>
     </div>
   </section>
@@ -38,13 +37,25 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
   <section class="flex pt-4 pb-16 px-6 xl:px-48 lg:px-24 mx-auto ">
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
-        <p class="text-wrap text-lg">I have a real passion for helping all of my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful and meets your dreams! I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
-        <p class="text-wrap text-lg">I love quirky, interesting dresses - vintage inspired, colour and alternative styles especially dark and gothic. That said, I also adore classic looks, beautiful princess ballgowns and clean contemporary styles. I have a real eye for what suits you and will suggest different options to adapt a dress to work better for you. I will often pick out a wild card dress for you (sometimes it comes to me as soon as I meet you!) and many of my brides have actually found that it is the one.</p>
-        <p class="text-wrap text-lg">Basically, whatever is your dream dress, I want to help you achieve it and if you have no idea, then that's absolutely fine too.  We will find it.</p>
-        <?php include(get_template_directory(  ) . '/template-parts/global/book-appointment-btn.php'); ?>
+        <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">Creating your Dream Dress - Whatever your Style</h2>
+        <h3>Bespoke designed dresses at affordable prices. </h3>
+        <p class="text-wrap text-lg">The commissioned for you collection is exactly that. I have deliberately selected designers who are as sustainable as possible. They do not mass produce and only make dresses, by hand, to order. These designers enable me to offer customisable dresses and to be fully inclusive by being available to order in every size without restrictions.  </p>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
         <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_3')); ?>" alt="lorem10" class="w-full h-full object-cover">
+      </div>
+    </div>
+  </section>
+
+  <section class="flex pt-4 pb-8 px-6 xl:px-48 lg:px-24 mx-auto ">
+    <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
+      <div class="bg-grey-2000 h-full w-full lg:w-6/12 order-2 lg:order-1">
+        <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_2')); ?>" alt="lorem10" class="w-full h-full object-cover">
+      </div>
+      <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
+        <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">A Welcoming, Personal Experience You'll Never Forget</h2>
+        <h3>Everyone is welcome at Maidens and Ravens</h3>
+        <p class="text-wrap text-lg">I have a real passion for helping all of my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful and meets your dreams! I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
       </div>
     </div>
   </section>
