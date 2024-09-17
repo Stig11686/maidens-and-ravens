e8b5abafae3bb1a077c85bed4b0e93506a487b14
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-09-17 19:33:38 +0100

    remove accordion from single product page for unordered list

--- template-parts/content-single-product.php
+++ template-parts/content-single-product.php
@@ -10,9 +10,9 @@ $fields = get_fields($product_id);
             <div class="w-full md:w-1/2">
                 <img src="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ) ? wp_get_attachment_image_url( get_post_thumbnail_id(), 'full' ) : 'https://images.unsplash.com/photo-1622277430358-f4d134452e2e?q=80&w=2147&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; ?>" alt="<?php the_title(); ?>" class="w-full h-full object-cover">
             </div>
-            <div class="w-full md:w-1/2 flex flex-col gap-8 p-6 text-center">
-                <h2 class="text-4xl lg:text-5xl"><?php the_title(); ?></h2>
-                <p class="text-5xl lg:text-6xl font-bold"><?php echo $product->get_price_html(); ?></p>
+            <div class="w-full md:w-1/2 flex flex-col gap-8 p-6">
+                <h2 class="text-4xl lg:text-5xl text-center"><?php the_title(); ?></h2>
+                <p class="text-5xl lg:text-6xl font-bold text-center"><?php echo $product->get_price_html(); ?></p>
                 <?php
                 if($fields):
                 ?>
@@ -22,7 +22,7 @@ $fields = get_fields($product_id);
                             foreach ($fields as $field_name => $value) :
                                 if($value):
                         ?>
-                                <li class=""><span class="front-bold"><?php echo ucwords(str_replace('_', ' ', $field_name)); ?> :</span> <?php echo $value; ?></li>
+                                <li class=""><span class="font-bold"><?php echo ucwords(str_replace('_', ' ', $field_name)); ?> :</span> <?php echo $value; ?></li>
                                     
                                     
                         <?php
