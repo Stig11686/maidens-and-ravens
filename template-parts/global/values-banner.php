0855b0dcd8b217c793f18d3240e2777ae22bf1ba
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-01 21:51:54 +0100

    add values banner for use across pages

--- template-parts/global/values-banner.php
+++ template-parts/global/values-banner.php
@@ -1,14 +1,14 @@
 <div class="w-full bg-primary grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
     <div class="flex flex-col gap-2">
         <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
-        <div class="text-white">
+        <div class="text-white text-center">
             <h4 class="font-bold">No Size Limit</h4> 
             <p>We have dresses for everyone!</p>
         </div>
     </div>
     <div class="flex flex-col gap-2">
         <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
-        <div class="text-white">
+        <div class="text-white text-center">
         <h4 class="font-bold">Planet friendly wedding dresses</h4>
 
         <p>Sample dresses and made to order by sustainable designers.</p>
@@ -16,7 +16,7 @@
     </div>
     <div class="flex flex-col gap-2">
         <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
-        <div class="text-white">
+        <div class="text-white text-center">
         <h4 class="font-bold">Options to Customise </h4>
 
         <p>Choose your dress and make changes to suit your style </p>
@@ -24,7 +24,7 @@
     </div>
     <div class="flex flex-col gap-2">
         <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
-        <div class="text-white">
+        <div class="text-white text-center">
         <h4 class="font-bold">Affordability </h4>
 
         <p>Dresses for every budget </p>
