eaf4d81f75815dc0fabdaa834d62c11365a8bb04
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-01 21:20:01 +0100

    testimonial page

--- template-parts/content-testimonials.php
+++ template-parts/content-testimonials.php
@@ -1,5 +1,5 @@
 <figure class="rounded-2xl bg-card-bg p-6 shadow-lg ring-1 ring-gray-900/5">
     <blockquote class="">
-    <div class="text-white text-xl"><?php the_content(); ?></p>
+    <div class="text-xl"><?php the_content(); ?></p>
     </blockquote>
 </figure>
