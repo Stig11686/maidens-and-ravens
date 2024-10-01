c579a453d441cf0fcc0bc08db39d13ad17b7ed23
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-01 20:49:56 +0100

    add start to faq archive page

---
+++ archive-faqs.php
@@ -0,0 +1,8 @@
+<?php
+    include(get_template_directory(  ) . '/template-parts/global/header.php'); 
+    include(get_template_directory(  ) . '/template-parts/global/internal-hero.php'); 
+
+?>
+
+<?php
+    include(get_template_directory(  ) . '/template-parts/global/footer.php');
