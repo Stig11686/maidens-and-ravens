57f01b42dc7460905d824c0200eca39a38517b9c
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-01 20:55:25 +0100

    add start to faq archive page

--- archive-faqs.php
+++ archive-faqs.php
@@ -3,6 +3,40 @@
     include(get_template_directory(  ) . '/template-parts/global/internal-hero.php'); 
 
 ?>
+    <div class="px-4 max-w-7xl mx-auto">
+        <?php
+            while(have_posts()) : the_post(); 
+        ?>
+
+            <div class="py-4 bg-primary shadow-lg border px-6 w-full">
+                <div class="flex justify-between items-center self-stretch faq-btn">
+                    <div class="flex items-center gap-2">
+                        <div class="flex flex-col justify-center text-sm font-bold text-white"><?php the_title(); ?></div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <g clip-path="url(#clip0_85_3056)">
+                            <mask id="mask0_85_3056" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="2" y="2" width="20" height="20">
+                                <path d="M10.5714 10.5714V2H13.4286V10.5714H22V13.4286H13.4286V22H10.5714V13.4286H2V10.5714H10.5714Z" fill="black"/>
+                            </mask>
+                            <g mask="url(#mask0_85_3056)">
+                                <rect width="24" height="24" fill="#F9D6F7"/>
+                            </g>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_85_3056">
+                                <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                </div>
+                <div class="flex flex-col self-stretch gap-8 faq-content box-content bg-transparent text-white">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
 
 <?php
     include(get_template_directory(  ) . '/template-parts/global/footer.php');
