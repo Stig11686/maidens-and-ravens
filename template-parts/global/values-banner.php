b32fd8719bf9fc2066ef0b19994ac51305bee063
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-02 13:46:15 +0100

    fix size of icons on banner

--- template-parts/global/values-banner.php
+++ template-parts/global/values-banner.php
@@ -1,31 +1,31 @@
 <div class="w-full bg-primary grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 py-8">
-    <div class="flex flex-col gap-2">
-        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/scissors.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
+    <div class="flex flex-col gap-6">
+        <div class="h-20 w-20 mx-auto"><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/scissors.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
-            <h4 class="font-bold">No Size Limit</h4> 
+            <h4 class="font-bold text-xl">No Size Limit</h4> 
             <p>We have dresses for everyone!</p>
         </div>
     </div>
-    <div class="flex flex-col gap-2">
-        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/eco-friendly.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
+    <div class="flex flex-col gap-6">
+        <div class="h-20 w-20 mx-auto"><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/eco-friendly.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
-        <h4 class="font-bold">Planet friendly wedding dresses</h4>
+        <h4 class="font-bold text-xl">Planet friendly wedding dresses</h4>
 
         <p>Sample dresses and made to order by sustainable designers.</p>
         </div>
     </div>
-    <div class="flex flex-col gap-2">
-        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/needle-with-thread.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
+    <div class="flex flex-col gap-6">
+        <div class="h-20 w-20 mx-auto"><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/needle-with-thread.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
-        <h4 class="font-bold">Options to Customise </h4>
+        <h4 class="font-bold text-xl">Options to Customise </h4>
 
         <p>Choose your dress and make changes to suit your style </p>
         </div>
     </div>
-    <div class="flex flex-col gap-4">
-        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/dress-wedding-dress.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
+    <div class="flex flex-col gap-6">
+        <div class="h-20 w-20 mx-auto"><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/wedding-dress.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
-        <h4 class="font-bold">Affordability </h4>
+        <h4 class="font-bold text-xl">Affordability </h4>
 
         <p>Dresses for every budget </p>
         </div>
