f8bf72fdbce77fbeb761525fc1ee299f00038143
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-02 12:37:24 +0100

    testing values banner images

--- template-parts/global/values-banner.php
+++ template-parts/global/values-banner.php
@@ -1,13 +1,13 @@
 <div class="w-full bg-primary grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 py-8">
     <div class="flex flex-col gap-2">
-        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/dress-wedding-dress.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
+        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/scissors.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
             <h4 class="font-bold">No Size Limit</h4> 
             <p>We have dresses for everyone!</p>
         </div>
     </div>
     <div class="flex flex-col gap-2">
-        <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
+        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/eco-friendly.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
         <h4 class="font-bold">Planet friendly wedding dresses</h4>
 
@@ -15,7 +15,7 @@
         </div>
     </div>
     <div class="flex flex-col gap-2">
-        <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
+        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/needle-with-thread.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
         <h4 class="font-bold">Options to Customise </h4>
 
@@ -23,7 +23,7 @@
         </div>
     </div>
     <div class="flex flex-col gap-4">
-        <div><img src="" aria-hidden="true" class="w-full h-full object-cover" /></div>
+        <div><img src="<?php echo get_template_directory_uri(  ) . '/img/icons/dress-wedding-dress.svg' ?>" aria-hidden="true" class="w-full h-full object-cover" /></div>
         <div class="text-white text-center">
         <h4 class="font-bold">Affordability </h4>
 
