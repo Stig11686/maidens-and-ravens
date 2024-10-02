fcd5937ecf635dc13105248ca11879e74b5b5ad5
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-02 16:45:38 +0100

    change ordering on about elizabeth page

--- templates/about-elizabeth.php
+++ templates/about-elizabeth.php
@@ -16,7 +16,7 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
         <p class="text-wrap text-lg">I started my journey into Bridalwear in 2019 but before that I always had a love for art, making things, sewing, knitting and vintage clothing. Growing up in Edinburgh, I enjoyed charity shop snooping and loved buying dresses from the 60s and 70s which I would get tailored to me. Like all of my friends, my prom dress was a 1980s bridesmaid dress - mint green with puff sleeves and lace! I wanted to look like Sarah from Labyrinth in the ballroom scene with David Bowie, complete with ribbons in my hair! I still love that look.</p>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
-        <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_1')); ?>" alt="lorem10" class="w-full h-full object-cover">
+        <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_1')); ?>" alt="Image of Elizabeth at Maidens and Ravens Bridal Boutique York" class="w-full h-full object-cover">
       </div>
     </div>
   </section>
@@ -40,7 +40,6 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
         <p class="text-wrap text-lg">I have a real passion for helping all of my brides. I am a people person and am genuinely committed to helping you find a dress which makes you feel beautiful and meets your dreams! I firmly believe there is a dress for every bride (and a bride for every dress!). I love brides of every age, shape, size and colour. I pride myself in being fully inclusive and making your appointments as relaxed and enjoyable as possible.</p>
         <p class="text-wrap text-lg">I love quirky, interesting dresses - vintage inspired, colour and alternative styles especially dark and gothic. That said, I also adore classic looks, beautiful princess ballgowns and clean contemporary styles. I have a real eye for what suits you and will suggest different options to adapt a dress to work better for you. I will often pick out a wild card dress for you (sometimes it comes to me as soon as I meet you!) and many of my brides have actually found that it is the one.</p>
         <p class="text-wrap text-lg">Basically, whatever is your dream dress, I want to help you achieve it and if you have no idea, then that's absolutely fine too.  We will find it.</p>
-        <?php include(get_template_directory(  ) . '/template-parts/global/book-appointment-btn.php'); ?>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
         <img src="<?php echo esc_url(get_theme_mod('elizabeth_image_3')); ?>" alt="lorem10" class="w-full h-full object-cover">
