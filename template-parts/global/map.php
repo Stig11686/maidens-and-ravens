bbb46df6b8e72a1e24dd1adc988a911c4e6b2d46
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-02 16:51:38 +0100

    map width to 100%

--- template-parts/global/map.php
+++ template-parts/global/map.php
@@ -3,7 +3,7 @@
       <div class="flex flex-col items-start gap-8 decorated-list lg:order-2 lg:w-5/12">
         <h2 class="mb-0 text-3xl lg:text-5xl">Find the Boutique!</h2>
         <p class="text-lg">Fill out our form to book your appointment and we will be in touch to confirm your appointment and answer any questions you may have.</p>
-        <iframe width="600" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Micklegate,%20York+(Maidens%20and%20Ravens)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
       <div id="contact" class="bg-grey-2000 h-full w-full lg:w-6/12">
         <?php echo do_shortcode('[contact-form-7 id="080a418" title="Enquiry Form"]'); ?>
