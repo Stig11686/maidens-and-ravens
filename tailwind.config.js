108e4729a586f8c87b1ed6b1b575ac756597c4c2
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-04 15:22:37 +0100

    change primary colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         '3/4': '3/4',
       },
       colors: {
-        'primary': '#9C0D38',
+        'primary': '##431A2F',
         'secondary': '#F9D6F7',
         'neutral': '#EBCBB4',
         'gray': {
