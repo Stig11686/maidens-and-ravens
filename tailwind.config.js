5eab7be89e70a424310594f85aa0618d4369c3d5
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-07 12:39:05 +0100

    change burgundy for plum

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,8 @@ module.exports = {
         '3/4': '3/4',
       },
       colors: {
-        'primary': '#431A2F',
+        //'primary': '#431A2F',
+        'primary': '#974A87',
         'secondary': '#F9D6F7',
         'neutral': '#EBCBB4',
         'gray': {
