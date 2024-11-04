1ae874338381b888ed4273b1bde0b831b0554ef1
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-04 16:31:46 +0000

    remove tracking tight frominternal hero

--- template-parts/global/internal-hero.php
+++ template-parts/global/internal-hero.php
@@ -10,7 +10,7 @@ $title = is_archive() ? get_the_archive_title() : get_the_title();
       <div class="flex flex-col gap-2 md:flex-row md:items-center pt-8 lg:pt-0">
         <div class="mx-auto flex flex-col items-center gap-4 z-10 px-4">
           <div class="max-w-xl mx-auto flex flex-col items-start text-white text-center">
-            <h1 id="tagline" class="mb-0 leading-snug max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl xl:col-auto"><?php echo $title; ?></h1>
+            <h1 id="tagline" class="mb-0 leading-snug max-w-2xl text-4xl font-bold sm:text-6xl xl:col-auto"><?php echo $title; ?></h1>
           </div>
         </div>
       </div>
