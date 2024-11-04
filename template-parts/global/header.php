39f471a3dadc414d05c8d13ada05a358aba67b63
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-04 10:56:32 +0000

    add body classes

--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body class="<?php body_class(); ?>">
+<body <?php body_class(); ?>>
     <?php include(get_template_directory(  ) . '/template-parts/global/fixed-book-appointment-btn.php'); ?>
     <?php include(get_template_directory(  ) . '/template-parts/global/book-appt-modal.php'); ?>
     <?php include(get_template_directory(  ) . '/template-parts/nav.php'); ?>
