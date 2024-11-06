c8d08e7226431f9f6eb20217a1577d8b687985c4
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-06 19:01:37 +0000

    change testimonial style on archive page

--- template-parts/content-testimonials.php
+++ template-parts/content-testimonials.php
@@ -1,6 +1,10 @@
-<figure class="rounded-2xl bg-card-bg p-6 shadow-lg ring-1 ring-gray-900/5">
-    <blockquote class="flex flex-col gap-6">
-    <div class="text-xl"><?php the_content(); ?></div>
-    <div class="text-xl font-bold"><?php the_title(); ?></div>
+<figure class="rounded-2xl bg-gray-50 p-8 text-sm/6">
+    <blockquote class="text-gray-900">
+        <p><?php the_content(); ?></p>
     </blockquote>
-</figure>
+    <figcaption class="mt-6 flex items-center gap-x-4">
+        <img class="h-10 w-10 rounded-full bg-gray-50" src="<?php get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title() . ' testimonial of Maidens and Ravens bridal boutique in York'; ?>">
+        <div>
+        <div class="font-semibold text-gray-900"><?php the_title(); ?></div>
+        </div>
+</figcaption>
