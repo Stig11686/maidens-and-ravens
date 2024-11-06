ead4c10ff476251d50a14b9586b6025a4a45df76
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-06 22:28:28 +0000

    change boutique wording

--- templates/the-boutique.php
+++ templates/the-boutique.php
@@ -15,7 +15,7 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
     <div class="flex flex-col lg:flex-row lg:justify-between  gap-10 max-w-7xl mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
         <h2 class="mb-0 leading-120 text-3xl lg:text-5xl text-primary">The Boutique</h2>
-        <p class="leading-200 text-md">Our pretty little boutique is situated on historic Micklegate, through the bar walls, which is the traditional entrance to the city of York.</p>
+        <p class="leading-200 text-md">Maidens & Ravens is a pretty little boutique situated on historic Micklegate, through the bar walls, which is the traditional royal entrance to the city of York.</p>
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
         <img src="<?php echo esc_html($image1) ?>" alt="Maidens and Ravens wedding dress boutique in York" class="w-full h-full object-cover">
@@ -28,11 +28,11 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto">
       <div class="flex flex-col items-start gap-8 decorated-list lg:order-2 lg:w-5/12">
         <h2 class="mb-0 text-3xl lg:text-5xl text-primary">Maidens and Ravens, Micklegate, York</h2>
-        <p class="text-wrap text-md">The street is described as one of the most handsome in York and is home to beautiful book shops and lovely places to eat.  It has been used as a location for many period dramas due to the Georgian building and cobbles.</p>
-        <p>Small but perfectly formed, we are definitely not your traditional grey and white bridal shop! Our vibe is colourful and pretty, a bit vintage, with a little darkness and lots of curious details. We do not do minimalist style here! </p>
-        <p>When you book in with us you will have this beautiful space completely to yourself and our aim is to give you an experience to remember.
-        Like our dresses, we love to be unique, and this is definitely reflected in our choice of everything from our wallpaper to our lighting.
-        Look out for the ravens...</p>
+        <p class="text-wrap text-md">The street is described as one of the most handsome in York and is home to beautiful book shops and lovely places to eat. It has been used as a location for many period dramas due to the Georgian building and cobbles.</p>
+
+        <p>Small but perfectly formed, Maidens & Ravens is definitely not your traditional grey and white bridal shop! The vibe is colourful and pretty, a bit vintage, with a little darkness and lots of curious details. I do not do minimalist style here!</p>
+
+        <p>When you book in with me you will have this beautiful space completely to yourself and my aim is to give you an experience to remember. Like my dresses, I wanted the boutique to be unique, and this is definitely reflected in my choice of everything from the beautiful wallpaper to the lighting. Look out for the ravens...</p>
 
       </div>
       <div class="bg-grey-2000 h-full w-full lg:w-6/12">
@@ -45,8 +45,8 @@ include(get_template_directory() . '/template-parts/global/internal-hero.php');
     <div class="flex flex-col lg:flex-row lg:justify-between  items-start gap-10 max-w-7xl mx-auto"> 
       <div class="flex flex-col items-start gap-8 decorated-list lg:w-5/12">
           <h2 class="mb-0 text-3xl lg:text-5xl text-primary">Information and Accessibility</h2>
-          <p class="text-wrap text-md">Our nearest carpark is Nunnery Lane, and we are about 5 minutes’ walk from the train station.
-              If driving from out of York, the park and ride is advisable.
+          <p class="text-wrap text-md">The nearest carpark to Maidens & Ravens is Nunnery Lane, and we are about 5 minutes’ walk from the train station. If driving from out of York, the park and ride is advisable.
+
           </p>
           <h3 class="mb-0 text-2xl lg:text-3xl">Accessibility</h2>
 
