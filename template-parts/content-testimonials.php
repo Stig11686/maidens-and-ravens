25ab354ad7faae89d79e9ade613e0bb0cd6fbae0
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-06 19:08:16 +0000

    new layout testimonials archive page

--- template-parts/content-testimonials.php
+++ template-parts/content-testimonials.php
@@ -1,12 +1,12 @@
 <div class="pt-8 sm:inline-block sm:w-full sm:px-4">
     <figure class="rounded-2xl bg-gray-50 p-8 text-sm/6">
         <blockquote class="text-gray-900">
-            <p><?php the_content(); ?></p>
+            <p><?php echo the_content(); ?></p>
         </blockquote>
         <figcaption class="mt-6 flex items-center gap-x-4">
-            <img class="h-10 w-10 rounded-full bg-gray-50" src="<?php get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title() . ' testimonial of Maidens and Ravens bridal boutique in York'; ?>">
+            <img class="h-10 w-10 rounded-full bg-gray-50" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title() . ' testimonial of Maidens and Ravens bridal boutique in York'; ?>">
             <div>
-            <div class="font-semibold text-gray-900"><?php the_title(); ?></div>
+            <div class="font-semibold text-gray-900"><?php echo the_title(); ?></div>
             </div>
         </figcaption>
     </figure>
