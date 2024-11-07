85a53d4d9f86ba952fa12a93b6d220f3a3e99065
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-07 10:56:45 +0000

    move breadcrumbs up

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -70,7 +70,7 @@ if ( woocommerce_product_loop() ) {
 			 */
 			do_action( 'woocommerce_shop_loop' );
 
-			wc_get_template_part( 'content', 'product' );
+			get_template_part( 'content', 'product' );
 		}
 	}
 
