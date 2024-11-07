294d9577c2dfa9629fdd35d61976d2394a20bed9
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-11-07 21:12:09 +0000

    try and keep filters on page when no results appear

--- woocommerce/taxonomy-product-cat.php
+++ woocommerce/taxonomy-product-cat.php
@@ -1,66 +1,62 @@
 <?php
 
-    include(get_template_directory() . '/template-parts/global/header.php');
-    include(get_template_directory() . '/woocommerce/template-parts/archive-hero.php');
-    do_action( 'woocommerce_before_main_content' );
+include(get_template_directory() . '/template-parts/global/header.php');
+include(get_template_directory() . '/woocommerce/template-parts/archive-hero.php');
+do_action( 'woocommerce_before_main_content' );
 
 ?>
-    <div class="max-w-7xl mx-auto py-16 px-6">
-        <div class="flex flex-col items-start gap-4 mb-6">
-            <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-                <h2 class="text-3xl lg:text-5xl"><?php woocommerce_page_title(); ?></h2>
-            <?php endif; ?>
+<div class="max-w-7xl mx-auto py-16 px-6">
+    <div class="flex flex-col items-start gap-4 mb-6">
+        <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+            <h2 class="text-3xl lg:text-5xl"><?php woocommerce_page_title(); ?></h2>
+        <?php endif; ?>
 
+        <?php
+        do_action( 'woocommerce_archive_description' );
+        do_action( 'woocommerce_before_shop_loop' );
+        ?>
+    </div>
+
+    <div class="flex flex-col gap-6 md:flex-row">
+        <div class="w-full md:w-3/12">
+            <?php include(get_template_directory() . '/template-parts/filters.php'); ?>
+        </div>
+
+        <div class="w-full md:w-9/12">
             <?php
-            do_action( 'woocommerce_archive_description' );
-            do_action( 'woocommerce_before_shop_loop' );
+            if ( woocommerce_product_loop() ) {
+
+                woocommerce_product_loop_start();
+
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    echo '<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">';
+                    while ( have_posts() ) {
+                        the_post();
+
+                        /**
+                         * Hook: woocommerce_shop_loop.
+                         */
+                        do_action( 'woocommerce_shop_loop' );
+
+                        get_template_part( 'template-parts/content', 'product' );
+                    }
+                    echo '</div>';
+                }
+
+                woocommerce_product_loop_end();
+
+                do_action( 'woocommerce_after_shop_loop' );
+
+            } else {
+                // No products found, display no-products message
+                do_action( 'woocommerce_no_products_found' );
+            }
             ?>
         </div>
-        <div class="flex flex-col gap-6 md:flex-row">
-        <div class="w-full md:w-3/12">
-            <?php include(get_template_directory(  ) . '/template-parts/filters.php'); ?>
-        </div>
-        <div class="w-full md:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-
-<?php
-if ( woocommerce_product_loop() ) {
-
-    woocommerce_product_loop_start();
-
-        if ( wc_get_loop_prop( 'total' ) ) {
-            while ( have_posts() ) {
-                the_post();
-
-                /**
-                 * Hook: woocommerce_shop_loop.
-                 */
-                do_action( 'woocommerce_shop_loop' );
-
-                get_template_part( 'template-parts/content', 'product' );
-            }
-        ?>
-        </div>
-        
-
-    <?php
-
-    }
-
-    woocommerce_product_loop_end();
-    //woocommerce_pagination();
-
-    do_action( 'woocommerce_after_shop_loop' );
-} else {
-    /**
-     * Hook: woocommerce_no_products_found.
-     */
-    do_action( 'woocommerce_no_products_found' );
-}
-?>
+    </div>
 </div>
 
 <?php
 
-//get_footer( 'shop' );
-include(get_template_directory(  ) . '/template-parts/global/footer.php');
+include(get_template_directory() . '/template-parts/global/footer.php');
 ?>
